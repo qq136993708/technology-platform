8c7b076af2d155d8e048fe41facd56608cd904b9
Administrator <Administrator@PC-20180809QFBY>
2018-11-20 08:50:17 +0800

    fix bug

--- pcitc-stp/src/main/java/com/pcitc/web/controller/intlProject/IntlProjectNoticeController.java
+++ pcitc-stp/src/main/java/com/pcitc/web/controller/intlProject/IntlProjectNoticeController.java
@@ -102,7 +102,7 @@ public class IntlProjectNoticeController extends BaseController {
 		}
 	}
 
-	@RequestMapping(value = "/project/sent-notice")
+	@RequestMapping(value = "/intl_project/sent-notice")
 	public Object sentProjectNotice(@RequestParam(value = "noticeId", required = true) String noticeId) throws Exception {
 		ResponseEntity<Integer> status = this.restTemplate.exchange(PROJECT_NOTICE_SENT + noticeId, HttpMethod.POST, new HttpEntity<Object>(this.httpHeaders), Integer.class);
 		return status.getBody();
