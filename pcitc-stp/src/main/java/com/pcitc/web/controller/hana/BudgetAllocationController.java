773daaefda1e5727992a83aa3c2102cecdd1e8f4
admin <admin@admin-PC>
2018-11-20 18:49:54 +0800

    chart

--- pcitc-stp/src/main/java/com/pcitc/web/controller/hana/BudgetAllocationController.java
+++ pcitc-stp/src/main/java/com/pcitc/web/controller/hana/BudgetAllocationController.java
@@ -39,6 +39,12 @@ public class BudgetAllocationController {
 	
 	private static final String jtgszbkjjfys_data = "http://pcitc-zuul/system-proxy/out-project-provider/budget-proposals/group-company/stp-money";
 	private static final String gfgszbkjjfysjyg_data = "http://pcitc-zuul/system-proxy/out-decision-provider/ysfp/jfysfx/stock-company-money";
+	private static final String cbmkjjfyszb_data = "http://pcitc-zuul/system-proxy/out-decision-provider/ysfp/jfysfx/department-budget";
+	
+	
+	
+	
+	
 	
 	  
 	    //集团公司总部科技经费预算----（建议稿）
@@ -90,21 +96,24 @@ public class BudgetAllocationController {
 	  @RequestMapping(method = RequestMethod.GET, value = "/ba/zcgszbkjxmjfys")
 	  public String zcgszbkjxmjfys(HttpServletRequest request) throws Exception
 	  {
-		    
+		  String month = HanaUtil.getCurrrentYear();
+			request.setAttribute("year", month);
 	        return "stp/hana/budgetAllocation/zcgszbkjxmjfys";
 	  }
 	//股份公司总部科技经费预算（调整稿）
 	  @RequestMapping(method = RequestMethod.GET, value = "/ba/gfgszbkjjfystzg")
 	  public String gfgszbkjjfystzg(HttpServletRequest request) throws Exception
 	  {
-		    
+		  String month = HanaUtil.getCurrrentYear();
+			request.setAttribute("month", month);
 	        return "stp/hana/budgetAllocation/gfgszbkjjfystzg";
 	  }
 	//股份公司总部科技经费预算（建议稿）
 	  @RequestMapping(method = RequestMethod.GET, value = "/ba/gfgszbkjjfysjyg")
 	  public String gfgszbkjjfysjyg(HttpServletRequest request) throws Exception
 	  {
-		    
+		  String month = HanaUtil.getCurrrentYear();
+			request.setAttribute("year", month);
 	        return "stp/hana/budgetAllocation/gfgszbkjjfysjyg";
 	  }
 	  
@@ -148,9 +157,47 @@ public class BudgetAllocationController {
 	  @RequestMapping(method = RequestMethod.GET, value = "/ba/cbmkjjfyszb")
 	  public String cbmkjjfyszb(HttpServletRequest request) throws Exception
 	  {
+		  
+		  
+			String month = HanaUtil.getCurrrentYear();
+			request.setAttribute("year", month);
 		    
 	        return "stp/hana/budgetAllocation/cbmkjjfyszb";
 	  }
+	  
+	  
+	  @RequestMapping(method = RequestMethod.GET, value = "/ba/cbmkjjfyszb_data")
+		@ResponseBody
+		public String cbmkjjfyszb_data(@ModelAttribute("param") LayuiTableParam param, HttpServletRequest request, HttpServletResponse response) {
+
+	    	PageResult pageResult = new PageResult();
+	    	String month = CommonUtil.getParameter(request, "month", "" + DateUtil.dateToStr(new Date(), DateUtil.FMT_MM));
+			String companyCode = CommonUtil.getParameter(request, "companyCode", "");
+			Map<String, Object> paramsMap = new HashMap<String, Object>();
+			paramsMap.put("month", month);
+			paramsMap.put("companyCode", companyCode);
+			JSONObject jsonObject = JSONObject.parseObject(JSONObject.toJSONString(paramsMap));
+			HttpEntity<String> entity = new HttpEntity<String>(jsonObject.toString(), httpHeaders);
+			
+			ResponseEntity<JSONArray> responseEntity = restTemplate.exchange(cbmkjjfyszb_data, HttpMethod.POST, entity, JSONArray.class);
+			int statusCode = responseEntity.getStatusCodeValue();
+			if (statusCode == 200)
+			{
+				
+				JSONArray	jSONArray = responseEntity.getBody();
+				pageResult.setData(jSONArray);
+				pageResult.setCode(0);
+				pageResult.setCount(Long.valueOf(jSONArray.size()));
+				pageResult.setLimit(1000);
+				pageResult.setPage(1l);
+				
+			}
+			JSONObject result = JSONObject.parseObject(JSONObject.toJSONString(pageResult));
+			System.out.println(">>>>>>>>>>>>>处部门科技经费预算总表 gfgszbkjjfysjyg_data:" + result.toString());
+			return result.toString();
+		}
+	  
+	  
 	//集团经费预算明细表
 	  @RequestMapping(method = RequestMethod.GET, value = "/ba/jtjfysmxb")
 	  public String jtjfysmxb(HttpServletRequest request) throws Exception
