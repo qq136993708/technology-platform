faed7ce05ba1bf242e65679cb097b5fc5dd5d2f7
uuy <uuy@DESKTOP-FLDRE7B>
2018-12-11 15:58:36 +0800

    股份公司总部科技经费预算

---
+++ pcitc-stp/src/main/java/com/pcitc/web/controller/out/OutDecisionController.java
@@ -0,0 +1,10 @@
+package com.pcitc.web.controller.out;
+
+import org.springframework.stereotype.Controller;
+
+import com.pcitc.web.common.BaseController;
+
+@Controller
+public class OutDecisionController extends BaseController {
+
+}
