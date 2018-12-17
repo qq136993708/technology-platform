26a073f8a1764979e9cc3047f48ce40a841c7f24
uuy <uuy@DESKTOP-FLDRE7B>
2018-12-17 11:30:55 +0800

    ui upd

--- pcitc-stp/src/main/webapp/stp/hana/home/oneLevelMain/investment.js
+++ pcitc-stp/src/main/webapp/stp/hana/home/oneLevelMain/investment.js
@@ -400,13 +400,14 @@ function 	load_mony_line_dt_investment(url,id,title,subtext,yAxis,rotate)
 	option_dt_investment.grid={
 	        top:"12%",
 	        left: '0%',
-	        right: '5%',
+	        right: '6%',
 	        bottom: '12%',
 	        containLabel: true
 	    }
     if(rotate==undefined){
         option_dt_investment.xAxis=[{
             type: 'category',
+            boundaryGap : false,
             axisLabel:{
                 interval:0,//0：全部显示，1：间隔为1显示对应类目，2：依次类推，（简单试一下就明白了，这样说是不是有点抽象）
                 rotate:0,//倾斜显示，-：顺时针旋转，+或不写：逆时针旋转
@@ -415,9 +416,10 @@ function 	load_mony_line_dt_investment(url,id,title,subtext,yAxis,rotate)
     }else {
         option_dt_investment.xAxis=[{
             type: 'category',
+            boundaryGap : false,
             axisLabel:{
                 interval:0,//0：全部显示，1：间隔为1显示对应类目，2：依次类推，（简单试一下就明白了，这样说是不是有点抽象）
-                rotate:30,//倾斜显示，-：顺时针旋转，+或不写：逆时针旋转
+                rotate:-30,//倾斜显示，-：顺时针旋转，+或不写：逆时针旋转
             }
         }]
     }
