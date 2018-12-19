e39835f1c4e5ba194b2b7f3330769dc144716de9
admin <admin@admin-PC>
2018-12-19 17:01:26 +0800

    chart

--- pcitc-stp/src/main/java/com/pcitc/web/controller/hanaLeader/OneLevelMainController.java
+++ pcitc-stp/src/main/java/com/pcitc/web/controller/hanaLeader/OneLevelMainController.java
@@ -189,6 +189,71 @@ public class OneLevelMainController {
 		}
 		
 		
+
+		@RequestMapping(method = RequestMethod.GET, value = "/knowledge_pie_01")
+		@ResponseBody
+		public String knowledge_pie_01(HttpServletRequest request, HttpServletResponse response) throws Exception {
+			Result result = new Result();
+			
+			String nd = CommonUtil.getParameter(request, "nd", "" + DateUtil.dateToStr(new Date(), DateUtil.FMT_YYYY));
+			String type = CommonUtil.getParameter(request, "type", "");
+			
+			Map<String, Object> paramsMap = new HashMap<String, Object>();
+			paramsMap.put("nd", nd);
+			paramsMap.put("type", type);
+			JSONObject jsonObject = JSONObject.parseObject(JSONObject.toJSONString(paramsMap));
+			HttpEntity<String> entity = new HttpEntity<String>(jsonObject.toString(), httpHeaders);
+			if (!nd.equals(""))
+			{
+				ResponseEntity<JSONArray> responseEntity = restTemplate.exchange(knowledge_02, HttpMethod.POST, entity, JSONArray.class);
+				int statusCode = responseEntity.getStatusCodeValue();
+				if (statusCode == 200) 
+				{
+					
+					JSONArray jSONArray = responseEntity.getBody();
+				    System.out.println(">>>>>>>>>>>>>>knowledge_pie_01 jSONArray-> " + jSONArray.toString());
+					List<Knowledge> list = JSONObject.parseArray(jSONArray.toJSONString(), Knowledge.class);
+				
+					
+						ChartPieResultData pie = new ChartPieResultData();
+						List<ChartPieDataValue> dataList = new ArrayList<ChartPieDataValue>();
+						List<String> legendDataList = new ArrayList<String>();
+						for (int i = 0; i < list.size(); i++) {
+							Knowledge f2 = list.get(i);
+							String projectName = f2.getLx();
+							String value = "0";
+							if(type.equals("1"))
+							{
+								value =f2.getApplyCount();
+							}
+							
+							if(type.equals("2"))
+							{
+								value =f2.getAgreeCount();
+							}
+							
+							
+							legendDataList.add(projectName);
+							dataList.add(new ChartPieDataValue(Integer.valueOf(value), projectName));
+						}
+						pie.setDataList(dataList);
+						pie.setLegendDataList(legendDataList);
+						result.setSuccess(true);
+						result.setData(pie);
+					
+				}
+				
+			} else
+			{
+				result.setSuccess(false);
+				result.setMessage("参数为空");
+			}
+			JSONObject resultObj = JSONObject.parseObject(JSONObject.toJSONString(result));
+			System.out.println(">>>>>>>>>>>>>>knowledge_pie_01 type= "+type+" : " + resultObj.toString());
+			return resultObj.toString();
+		}
+		
+		
 		@RequestMapping(method = RequestMethod.GET, value = "/knowledge_pie")
 		@ResponseBody
 		public String knowledge_pie(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -1798,8 +1863,13 @@ public class OneLevelMainController {
 												for(int i=0;i<list.size();i++)
 												{
 													SysNews sysNews=list.get(i);
-													sysNews.setCreatetimestr(sysNews.getCreatetime());
-													
+													String str="";
+													String createtime=sysNews.getCreatetime();
+													if(createtime!=null)
+													{
+														str=DateUtil.dateToStr(DateUtil.strToDate(sysNews.getCreatetime(), DateUtil.FMT_DD) , DateUtil.FMT_DD);
+													}
+													sysNews.setCreatetimestr(str);
 												}
 												result.setSuccess(true);
 												result.setData(list);
