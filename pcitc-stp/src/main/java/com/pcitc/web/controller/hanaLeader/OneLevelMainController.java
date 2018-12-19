cb4907ba57c146b7bbc7b9fc57c8c6df459bb2e9
admin <admin@admin-PC>
2018-12-19 11:22:42 +0800

    chart

--- pcitc-stp/src/main/java/com/pcitc/web/controller/hanaLeader/OneLevelMainController.java
+++ pcitc-stp/src/main/java/com/pcitc/web/controller/hanaLeader/OneLevelMainController.java
@@ -1609,28 +1609,9 @@ public class OneLevelMainController {
 												JSONArray jSONArray = responseEntity.getBody();
 												System.out.println(">>>>>>>>>>>>investment_01_01 jSONArray>>> " + jSONArray.toString());
 												List<BudgetMysql> list = JSONObject.parseArray(jSONArray.toJSONString(), BudgetMysql.class);
-												for(int i=0;i<list.size();i++)
-												{
-													BudgetMysql budgetMysql=list.get(i);
-													Object fyxRate= budgetMysql.getFyxRate();
-													Object jeRate= budgetMysql.getJeRate();
-													Object zbxRate= budgetMysql.getZbxRate();
-													if(fyxRate==null)
-													{
-														fyxRate=0;
-													}
-													if(jeRate==null)
-													{
-														jeRate=0;
-													}
-													if(zbxRate==null)
-													{
-														zbxRate=0;
-													}
-													budgetMysql.setFyxRate(fyxRate);;
-													budgetMysql.setJeRate(jeRate);
-													budgetMysql.setZbxRate(zbxRate);
-												}
+												
+												
+												
 												pageResult.setData(list);
 												pageResult.setCode(0);
 												pageResult.setCount(Long.valueOf(list.size()));
@@ -1762,7 +1743,7 @@ public class OneLevelMainController {
 												for(int i=0;i<list.size();i++)
 												{
 													SysNews sysNews=list.get(i);
-													sysNews.setCreatetimestr(DateUtil.dateToStr(sysNews.getCreatetime(), DateUtil.FMT_DD));
+													sysNews.setCreatetimestr(sysNews.getCreatetime());
 													
 												}
 												result.setSuccess(true);
