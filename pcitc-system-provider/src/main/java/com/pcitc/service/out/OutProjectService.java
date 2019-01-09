ed9c31f42b8b4d689d2b8eb4b45be15022c4080d
t-haifeng.zhang <t-haifeng.zhang@pcitc.com>
2019-01-09 15:24:32 +0800

    十条龙查询维度

--- pcitc-system-provider/src/main/java/com/pcitc/service/out/OutProjectService.java
+++ pcitc-system-provider/src/main/java/com/pcitc/service/out/OutProjectService.java
@@ -357,9 +357,14 @@ public interface OutProjectService {
 	
 	
 	/**
-	 * 获取详细的查询条件
+	 * 获取项目详情的查询维度（条件）
 	 */
 	public List getProjectInfoSelectCondition(HashMap<String, String> map);
+	
+	/**
+	 * 获取十条龙详情的查询维度（条件）
+	 */
+	public List getDragonProjectInfoSelectCondition(HashMap<String, String> map);
 	
 	/**
      * @param nd
