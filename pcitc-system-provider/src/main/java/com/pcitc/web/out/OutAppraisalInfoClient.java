eb0255b6cd849c8364e5f65c0458cd0d84d46946
t-haifeng.zhang <t-haifeng.zhang@pcitc.com>
2019-01-14 17:22:44 +0800

    成果接口调整（去掉外部单位）

--- pcitc-system-provider/src/main/java/com/pcitc/web/out/OutAppraisalInfoClient.java
+++ pcitc-system-provider/src/main/java/com/pcitc/web/out/OutAppraisalInfoClient.java
@@ -326,42 +326,36 @@ public class OutAppraisalInfoClient {
 			temMap.put("define3", "集团单位");
 			temList.add(2, temMap);
 		}
-		if (!JSON.toJSONString(temList).contains("外部单位")) {
-			HashMap<String, Object> temMap = new HashMap<String, Object>();
-			temMap.put("sl", 0);
-			temMap.put("define3", "外部单位");
-			temList.add(3, temMap);
-		}
 		
 		if (!JSON.toJSONString(temList).contains("休斯顿研发中心")) {
 			HashMap<String, Object> temMap = new HashMap<String, Object>();
 			temMap.put("sl", 0);
 			temMap.put("define3", "休斯顿研发中心");
-			temList.add(4, temMap);
+			temList.add(3, temMap);
 		}
 		if (!JSON.toJSONString(temList).contains("中东研发中心")) {
 			HashMap<String, Object> temMap = new HashMap<String, Object>();
 			temMap.put("sl", 0);
 			temMap.put("define3", "中东研发中心");
-			temList.add(5, temMap);
+			temList.add(4, temMap);
 		}
 		if (!JSON.toJSONString(temList).contains("集团公司")) {
 			HashMap<String, Object> temMap = new HashMap<String, Object>();
 			temMap.put("sl", 0);
 			temMap.put("define3", "集团公司");
-			temList.add(6, temMap);
+			temList.add(5, temMap);
 		}
 		if (!JSON.toJSONString(temList).contains("资产公司")) {
 			HashMap<String, Object> temMap = new HashMap<String, Object>();
 			temMap.put("sl", 0);
 			temMap.put("define3", "资产公司");
-			temList.add(7, temMap);
+			temList.add(6, temMap);
 		}
 		if (!JSON.toJSONString(temList).contains("盈科")) {
 			HashMap<String, Object> temMap = new HashMap<String, Object>();
 			temMap.put("sl", 0);
 			temMap.put("define3", "盈科");
-			temList.add(8, temMap);
+			temList.add(7, temMap);
 		}
 		JSONArray json = JSONArray.parseArray(JSON.toJSONString(temList));
 		return json;
