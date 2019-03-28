d9d9a1d1aff5c57ab4b791a8579b12a989a0fe1a
uuy <uuy@DESKTOP-FLDRE7B>
2019-03-28 16:55:33 +0800

    基础数据定义

--- pcitc-stp-provider/src/main/java/com/pcitc/common/BudgetSplitEnum.java
+++ pcitc-stp-provider/src/main/java/com/pcitc/common/BudgetSplitEnum.java
@@ -6,52 +6,54 @@ package com.pcitc.common;
  */
 public enum BudgetSplitEnum 
 {
-	SPLIT_GROUP_YF(101,"油服"),
-	SPLIT_GROUP_JX(102,"机械"),
-	SPLIT_GROUP_QT(103,"其他"),
+	SPLIT_GROUP_YF(101,"YF","油服"),
+	SPLIT_GROUP_JX(102,"JX","机械"),
+	SPLIT_GROUP_QT(103,"QT","其他"),
 	
-	SPLIT_ASSET_LY(201,"资产部"),
-	SPLIT_ASSET_HG(202,"化工部"),
+	SPLIT_ASSET_LY(201,"HGB","资产部"),
+	SPLIT_ASSET_HG(202,"ZCB","化工部"),
 		
-	SPLIT_STOCK_YJY(301,"股份付直属院"),
-	SPLIT_STOCK_YJY_KTY(3011,"勘探院"),
-	SPLIT_STOCK_YJY_GCY(3012,"工程院"),
-	SPLIT_STOCK_YJY_WTY(3013,"物探院"),
-	SPLIT_STOCK_YJY_SKY(3014,"石科院"),
-	SPLIT_STOCK_YJY_FSY(3015,"抚顺院"),
-	SPLIT_STOCK_YJY_BHY(3016,"北化院"),
-	SPLIT_STOCK_YJY_SHY(3017,"上海院"),
-	SPLIT_STOCK_YJY_AGY(3018,"安工院"),
-	SPLIT_STOCK_FZGS(302,"股份付分子公司"),
-	SPLIT_STOCK_JTDW(303,"股份付集团单位"),
-	SPLIT_STOCK_WBDW(304,"股份付外部单位"),
-	SPLIT_STOCK_YK(305,"股份付盈科"),
-	SPLIT_STOCK_XSD(306,"股份付休斯顿"),
-	SPLIT_STOCK_ZD(307,"股份付中东");						
+	SPLIT_STOCK_YJY(301,"GFFZSY","股份付直属院"),
+	SPLIT_STOCK_YJY_KTY(3011,"ZSY_KTY","勘探院"),
+	SPLIT_STOCK_YJY_GCY(3012,"ZSY_GCY","工程院"),
+	SPLIT_STOCK_YJY_WTY(3013,"ZSY_WTY","物探院"),
+	SPLIT_STOCK_YJY_SKY(3014,"ZSY_SKY","石科院"),
+	SPLIT_STOCK_YJY_FSY(3015,"ZSY_FSY","抚顺院"),
+	SPLIT_STOCK_YJY_BHY(3016,"ZSY_BHY","北化院"),
+	SPLIT_STOCK_YJY_SHY(3017,"ZSY_SHY","上海院"),
+	SPLIT_STOCK_YJY_AGY(3018,"ZSY_AGY","安工院"),
+	SPLIT_STOCK_FZGS(302,"GFFFZGS","股份付分子公司"),
+	SPLIT_STOCK_JTDW(303,"GFFJTDW","股份付集团单位"),
+	SPLIT_STOCK_WBDW(304,"GFFWBDW","股份付外部单位"),
+	SPLIT_STOCK_YK(305,"GFFYK","股份付盈科"),
+	SPLIT_STOCK_XSD(306,"GFFXSD","股份付休斯顿"),
+	SPLIT_STOCK_ZD(307,"GFFZD","股份付中东");						
 	
 	
-	private BudgetSplitEnum(Integer code, String desc) {
+	private BudgetSplitEnum(Integer id,String code, String name) {
+		this.id = id;
 		this.code = code;
-		this.desc = desc;
+		this.name = name;
 	}
-
-	private Integer code;
-	private String desc;
-
-	public Integer getCode() {
+	private Integer id;
+	private String code;
+	private String name;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getCode() {
 		return code;
 	}
-
-	public void setCode(Integer code) {
+	public void setCode(String code) {
 		this.code = code;
 	}
-
-	public String getDesc() {
-		return desc;
+	public String getName() {
+		return name;
 	}
-
-	public void setDesc(String desc) {
-		this.desc = desc;
+	public void setName(String name) {
+		this.name = name;
 	}
-
 }
