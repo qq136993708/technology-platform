447a35bbe9b7e3c769494743b87f12cedba92181
t-haifeng.zhang <t-haifeng.zhang@pcitc.com>
2019-03-30 15:05:41 +0800

    会签调整

--- pcitc-system-provider/src/main/java/com/pcitc/service/workflow/impl/SignService.java
+++ pcitc-system-provider/src/main/java/com/pcitc/service/workflow/impl/SignService.java
@@ -1,10 +1,13 @@
 package com.pcitc.service.workflow.impl;
 
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.activiti.engine.impl.pvm.delegate.ActivityExecution;
+import org.activiti.engine.task.IdentityLink;
+import org.activiti.engine.task.IdentityLinkType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
@@ -16,47 +19,48 @@ import com.pcitc.base.system.SysRole;
 import com.pcitc.base.system.SysRoleExample;
 import com.pcitc.base.system.SysUserRole;
 import com.pcitc.base.system.SysUserRoleExample;
+import com.pcitc.base.util.StrUtil;
 import com.pcitc.mapper.system.SysRoleMapper;
+import com.pcitc.mapper.system.SysUserMapper;
 import com.pcitc.mapper.system.SysUserRoleMapper;
 
+// 流程图中涉及到会签的时候，获取
 @Service("signService")
-@CacheConfig(cacheNames = "signCache")
+//@CacheConfig(cacheNames = "signCache")
 @Transactional(propagation = Propagation.REQUIRED, readOnly = false, rollbackFor = Exception.class)
 public class SignService {
-	
+
 	@Autowired
 	private SysRoleMapper roleMapper;
-	
+
 	@Autowired
 	private SysUserRoleMapper userRoleMapper;
 
-	@Cacheable(key = "'sign_'+#role", value = "signCache")
-	public Set<String> getUsers(ActivityExecution execution, String role) {
+	@Autowired
+	private SysUserMapper sysUserMapper;
+
+	// group_id 对应act视图里面的act_id_membership中的group_id
+	//@Cacheable(key = "'sign_'+#role", value = "signCache")
+	public Set<String> getUsers(ActivityExecution execution, String group_id) {
 		System.out.println("0=============CountersignService=getUsers");
-		System.out.println("1=============CountersignService=getUsers==========="+role);
+		System.out.println("1=============CountersignService=getUsers===========" + group_id);
 		Set<String> userIds = new LinkedHashSet<String>();
-		String nodeId=execution.getActivity().getId();
-		if (execution.getVariable("signUser_"+nodeId) == null) {
-			System.out.println("2=============CountersignService=getUsers==="+nodeId);
-			
+		String nodeId = execution.getActivity().getId();
+		if (execution.getVariable("signUser_" + nodeId) == null) {
+			System.out.println("2=============CountersignService=getUsers===" + nodeId);
 		}
-		SysRoleExample rexample = new SysRoleExample();
-		// 角色编码
-		rexample.createCriteria().andRoleFlagEqualTo(role);
-		List<SysRole> roleList = roleMapper.selectByExample(rexample);
-		if (roleList != null && roleList.size() > 0) {
-			SysUserRoleExample example = new SysUserRoleExample();
-			example.createCriteria().andRoleIdEqualTo(roleList.get(0).getRoleId());
-			List<SysUserRole> userRoles = userRoleMapper.selectByExample(example);
-			for (SysUserRole ur : userRoles) {
-				userIds.add(ur.getUserId());
-			}
+
+		// 分解group
+		String[] groups = group_id.split("-");
+		for (int i = 0; i < groups.length; i++) {
+			userIds.addAll(sysUserMapper.findUserByGroupIdFromACT(groups[i]));
 		}
-		
-		execution.setVariable("signUser_"+nodeId, userIds);		
-		System.out.println("3=============CountersignService=getUsers");
+
+		execution.setVariable("signUser_" + nodeId, userIds);
+		System.out.println("3=============CountersignService=getUsers"+userIds);
+		System.out.println("4=============CountersignService=getUsers"+userIds.size());
 		
 		return userIds;
 	}
-    
+
 }
