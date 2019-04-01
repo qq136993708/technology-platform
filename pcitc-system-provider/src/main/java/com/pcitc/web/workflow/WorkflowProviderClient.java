4950ae4ef326b0f88dfb52dad61433d28a24663f
t-haifeng.zhang <t-haifeng.zhang@pcitc.com>
2019-04-01 20:19:35 +0800

    会签时不指定具体人员

--- pcitc-system-provider/src/main/java/com/pcitc/web/workflow/WorkflowProviderClient.java
+++ pcitc-system-provider/src/main/java/com/pcitc/web/workflow/WorkflowProviderClient.java
@@ -10,8 +10,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamReader;
@@ -24,7 +26,19 @@ import org.activiti.engine.IdentityService;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.impl.RepositoryServiceImpl;
+import org.activiti.engine.impl.bpmn.behavior.ParallelMultiInstanceBehavior;
+import org.activiti.engine.impl.bpmn.behavior.UserTaskActivityBehavior;
+import org.activiti.engine.impl.javax.el.ExpressionFactory;
+import org.activiti.engine.impl.javax.el.ValueExpression;
+import org.activiti.engine.impl.juel.ExpressionFactoryImpl;
+import org.activiti.engine.impl.juel.SimpleContext;
+import org.activiti.engine.impl.persistence.entity.ExecutionEntity;
 import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
+import org.activiti.engine.impl.pvm.PvmActivity;
+import org.activiti.engine.impl.pvm.PvmTransition;
+import org.activiti.engine.impl.pvm.process.ActivityImpl;
+import org.activiti.engine.impl.task.TaskDefinition;
 import org.activiti.engine.repository.Model;
 import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.repository.ProcessDefinitionQuery;
@@ -53,6 +67,7 @@ import com.pcitc.base.util.StrUtil;
 import com.pcitc.base.workflow.ProcessDefVo;
 import com.pcitc.base.workflow.SysFunctionProdef;
 import com.pcitc.base.workflow.WorkflowVo;
+import com.pcitc.mapper.system.SysUserMapper;
 import com.pcitc.service.system.SysFileService;
 import com.pcitc.service.workflow.WorkflowInstanceService;
 import com.pcitc.utils.StringUtils;
@@ -79,16 +94,20 @@ public class WorkflowProviderClient {
 	@Autowired
 	SysFileService sysFileService;
 	
+	@Autowired
+	private SysUserMapper sysUserMapper;
+	
 	/**
 	 * @author zhf
 	 * @date 2019年3月26日 下午2:08:57
 	 */
 	@ApiOperation(value = "启动业务审批流程", notes = "公共审批流启动方法")
 	@RequestMapping(value = "/workflow-provider/common-workflow/start", method = RequestMethod.POST)
-	public String startCommonWorkflow(@RequestBody String jsonStr) {
+	public String startCommonWorkflow(@RequestBody String jsonStr) throws Exception {
 		if (jsonStr == null) {
 			return "流程启动异常,参数异常";
 		}
+		System.out.println("jsonStr---"+jsonStr);
 		JSONObject json = JSONObject.parseObject(jsonStr);
 		Map<String, Object> variables = new HashMap<String, Object>();
 		for (Map.Entry<String, Object> entry : json.entrySet()) {
@@ -168,6 +187,26 @@ public class WorkflowProviderClient {
 		// 本次任务的可用变量
 		Map<String, Object> taskVar = taskService.getVariables(task.getId());
 		
+		// 本次任务节点的下一个节点。特殊节点，根据表单内容来觉得下一步的审批人
+		Map<String, Object> temMap = new HashMap<String, Object>();
+		temMap.put("agree", "1");
+		TaskDefinition taskDef = this.getNextTaskInfo(processInstance.getId(), temMap);
+		// System.out.println("1=========TaskDefinition======="+taskDef);
+		if (taskDef != null && taskDef.getKey().startsWith("specialAuditor")) {
+			// 特殊节点，获取当初传递的值
+			Set<String> userIds = new LinkedHashSet<String>();
+			System.out.println("1=========TaskDefinition======="+taskDef.getKey());
+			System.out.println("1=========TaskDefinition======="+json.getString(taskDef.getKey()));
+			if (json.getString(taskDef.getKey()) != null) {
+				// 分解group
+				String[] groups = json.getString(taskDef.getKey()).toString().split("-");
+				for (int i = 0; i < groups.length; i++) {
+					userIds.addAll(sysUserMapper.findUserByGroupIdFromACT(groups[i]));
+				}
+			}
+			taskVar.put("auditor", userIds);
+		}
+		
 		// 插入本次任务的审批人姓名，方便下一步任务查询上一步执行人姓名
 		taskVar.put(task.getId(), json.getString("authenticatedUserName"));
 		// 流程的启动人姓名和流程发起时间
@@ -191,6 +230,12 @@ public class WorkflowProviderClient {
 			String[] userIdArr = json.getString("auditUserIds").split(",");
 			taskVar.put("assigneeList", Arrays.asList(userIdArr));
 			System.out.println("2会签时====" + Arrays.asList(userIdArr));
+		}
+		
+		if (json.getString("signAuditRate") != null && taskVar.get("auditor") != null) {
+			System.out.println("1会签时1====" + taskVar.get("auditor"));
+			taskVar.put("assigneeList", taskVar.get("auditor"));
+			System.out.println("2会签时2====" + taskVar.get("auditor"));
 		}
 		System.out.println("开始执行任务----------------"+task.getId());
 		taskService.complete(task.getId(), taskVar);
@@ -580,5 +625,160 @@ public class WorkflowProviderClient {
 		data.setCount((int) count);
 		System.out.println("2---------------------selectProcessDefList");
 		return data;
+	}
+	
+	
+	/**
+	 * 获取下一个用户任务信息
+	 * 
+	 * @param String
+	 *            taskId 任务Id信息
+	 * @return 下一个用户任务用户组信息
+	 * @throws Exception
+	 */
+	public TaskDefinition getNextTaskInfo(String processInstanceId, Map<String, Object> globalVar) throws Exception {
+
+		ProcessDefinitionEntity processDefinitionEntity = null;
+		String id = null;
+		TaskDefinition task = null;
+
+		// 获取流程发布Id信息
+		String definitionId = runtimeService.createProcessInstanceQuery().processInstanceId(processInstanceId).singleResult().getProcessDefinitionId();
+
+		processDefinitionEntity = (ProcessDefinitionEntity) ((RepositoryServiceImpl) repositoryService).getDeployedProcessDefinition(definitionId);
+
+		ExecutionEntity execution = (ExecutionEntity) runtimeService.createProcessInstanceQuery().processInstanceId(processInstanceId).singleResult();
+
+		// 当前流程节点Id信息
+		String activitiId = execution.getActivityId();
+		if (activitiId == null) {
+			return null;
+		}
+		// System.out.println("activitiId========"+activitiId);
+		// 获取流程所有节点信息
+		List<ActivityImpl> activitiList = processDefinitionEntity.getActivities();
+
+		// 遍历所有节点信息
+		for (ActivityImpl activityImpl : activitiList) {
+			id = activityImpl.getId();
+			if (activitiId.equals(id)) {
+				// 获取下一个节点信息
+				task = nextTaskDefinition(activityImpl, activityImpl.getId(), null, processInstanceId, globalVar);
+				break;
+			}
+		}
+		return task;
+	}
+
+	/**
+	 * 下一个任务节点信息,
+	 * 
+	 * 如果下一个节点为用户任务则直接返回,
+	 * 
+	 * 如果下一个节点为排他网关, 获取排他网关Id信息, 根据排他网关Id信息和execution获取流程实例排他网关Id为key的变量值,
+	 * 根据变量值分别执行排他网关后线路中的el表达式, 并找到el表达式通过的线路后的用户任务
+	 * 
+	 * @param ActivityImpl
+	 *            activityImpl 流程节点信息
+	 * @param String
+	 *            activityId 当前流程节点Id信息
+	 * @param String
+	 *            elString 排他网关顺序流线段判断条件
+	 * @param String
+	 *            processInstanceId 流程实例Id信息
+	 * @return
+	 */
+	private TaskDefinition nextTaskDefinition(ActivityImpl activityImpl, String activityId, String elString, String processInstanceId, Map<String, Object> globalVar) {
+
+		PvmActivity ac = null;
+
+		Object s = null;
+
+		// 如果遍历节点为用户任务并且节点不是当前节点信息
+		if ("userTask".equals(activityImpl.getProperty("type")) && !activityId.equals(activityImpl.getId())) {
+			// 获取该节点下一个节点信息(下一个实例可能是多实例节点)
+			System.out.println("activityImpl.getActivityBehavior()=================="+activityImpl.getActivityBehavior().getClass());
+			System.out.println("activityImpl.getActivityBehavior()=================="+activityImpl.getActivityBehavior().getClass().getName());
+			if (activityImpl.getActivityBehavior().getClass().getName().contains("UserTaskActivityBehavior")) {
+				TaskDefinition taskDefinition = ((UserTaskActivityBehavior) activityImpl.getActivityBehavior()).getTaskDefinition();
+				return taskDefinition;
+			} else if (activityImpl.getActivityBehavior().getClass().getName().contains("ParallelMultiInstanceBehavior")) {
+				ParallelMultiInstanceBehavior paralBehavior =  (ParallelMultiInstanceBehavior)activityImpl.getActivityBehavior();
+				return ((UserTaskActivityBehavior)paralBehavior.getInnerActivityBehavior()).getTaskDefinition();
+			} else {
+				// 其他特殊情况，暂时不考虑。目前只包含一般的用户任务和会签（多实例）任务
+				return null;
+			}
+
+		} else {
+			// 获取节点所有流向线路信息
+			List<PvmTransition> outTransitions = activityImpl.getOutgoingTransitions();
+			List<PvmTransition> outTransitionsTemp = null;
+			for (PvmTransition tr : outTransitions) {
+				ac = tr.getDestination(); // 获取线路的终点节点
+				// 如果流向线路为排他网关
+				if ("exclusiveGateway".equals(ac.getProperty("type"))) {
+					outTransitionsTemp = ac.getOutgoingTransitions();
+
+					// 如果排他网关只有一条线路信息
+					if (outTransitionsTemp.size() == 1) {
+						return nextTaskDefinition((ActivityImpl) outTransitionsTemp.get(0).getDestination(), activityId, elString, processInstanceId, globalVar);
+					} else if (outTransitionsTemp.size() > 1) { // 如果排他网关有多条线路信息
+						boolean cond = true;
+						for (PvmTransition tr1 : outTransitionsTemp) {
+							s = tr1.getProperty("conditionText"); // 获取排他网关线路判断条件信息
+							// System.out.println("1============================="+s+"------------------------------"+elString);
+							// 计算出现的变量的相应的表达式是否成立，el中可能有多个变量，在全局变量中分别取这些值
+							// 判断el表达式是否成立
+							// 可能存在除agree（同意不同意）之外的其他条件判断（一个节点只能有一个节点判断），所以直接遍历当前的网关条件
+							for (Map.Entry<String, Object> entry : globalVar.entrySet()) {
+								System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+								if (entry.getValue() != null && s != null && isCondition(entry.getKey(), s.toString().replaceAll(" ", ""), entry.getValue().toString())) {
+									cond = false;
+									// System.out.println("============================success-------------------"+elString);
+									return nextTaskDefinition((ActivityImpl) tr1.getDestination(), activityId, elString, processInstanceId, globalVar);
+								}
+							}
+						}
+						// 各个分支都不符合条件
+						if (cond)
+							return null;
+					}
+				} else if ("userTask".equals(ac.getProperty("type"))) {
+					System.out.println("userTask1---------------"+ac.getProperty("type"));
+					//return ((UserTaskActivityBehavior) ((ActivityImpl) ac).getActivityBehavior()).getTaskDefinition();
+					if(((ActivityImpl) ac).getActivityBehavior() instanceof UserTaskActivityBehavior){
+						UserTaskActivityBehavior aaBehavior = (UserTaskActivityBehavior)((ActivityImpl) ac).getActivityBehavior();
+						return ((UserTaskActivityBehavior)aaBehavior).getTaskDefinition();
+					} else if(((ActivityImpl) ac).getActivityBehavior() instanceof ParallelMultiInstanceBehavior){
+						ParallelMultiInstanceBehavior aaBehavior = (ParallelMultiInstanceBehavior)((ActivityImpl) ac).getActivityBehavior();
+						return ((UserTaskActivityBehavior)aaBehavior.getInnerActivityBehavior()).getTaskDefinition();
+					}
+				} else {
+					System.out.println("userTask2---------------"+ac.getProperty("type"));
+				}
+			}
+			return null;
+		}
+	}
+
+	/**
+	 * 根据key和value判断el表达式是否通过信息 el暂时不支持多条件的juel判断，所以画图的时候，多条件的用多个网关来区分
+	 * 
+	 * @param String
+	 *            key el表达式key信息
+	 * @param String
+	 *            el el表达式信息
+	 * @param String
+	 *            value el表达式传入值信息
+	 * @return
+	 */
+	public boolean isCondition(String key, String el, String value) {
+		// System.out.println(key+"===="+el+"========"+value);
+		ExpressionFactory factory = new ExpressionFactoryImpl();
+		SimpleContext context = new SimpleContext();
+		context.setVariable(key, factory.createValueExpression(value, String.class));
+		ValueExpression e = factory.createValueExpression(context, el, boolean.class);
+		return (Boolean) e.getValue(context);
 	}
 }
