17dfe246e054481cf0eb37fc5bca2fa9a27ccf5d
YTHT <YTHT@DESKTOP-JALPGIG>
2019-04-02 11:35:01 +0800

    装备台账明细实体类

---
+++ pcitc-stp-provider/src/main/java/com/pcitc/service/equipment/DetailService.java
@@ -0,0 +1,22 @@
+package com.pcitc.service.equipment;
+
+import com.pcitc.base.common.LayuiTableData;
+import com.pcitc.base.common.LayuiTableParam;
+
+public interface DetailService {
+	
+	
+	
+	/**===========================================装备台账=========================================*/
+
+	//装备台账分页方法
+	public LayuiTableData getDetailPage(LayuiTableParam param)throws Exception;
+	
+	
+	
+	
+	
+	
+	
+
+}
