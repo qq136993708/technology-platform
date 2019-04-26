749262d8b5b64a61b78bc1f66a78eca5ac0bc75c
guyabin <guyabin@DESKTOP-BBCVJSH>
2019-04-26 11:06:29 +0800

    1

--- pcitc-stp-provider/src/main/java/com/pcitc/service/equipment/SreScrapApplyService.java
+++ pcitc-stp-provider/src/main/java/com/pcitc/service/equipment/SreScrapApplyService.java
@@ -1,9 +1,12 @@
 package com.pcitc.service.equipment;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.pcitc.base.common.LayuiTableData;
 import com.pcitc.base.common.LayuiTableParam;
+import com.pcitc.base.common.Result;
 import com.pcitc.base.stp.equipment.SreProjectAudit;
 import com.pcitc.base.stp.equipment.SreScrapApply;
 
@@ -18,4 +21,6 @@ public interface SreScrapApplyService {
 	public int updateByPrimaryKeySelective(SreScrapApply record)throws Exception;
 	
 	public String addAudit(SreProjectAudit sreProjectAudit)throws Exception;
+	
+	Result dealInnerPurchaseFlow(String id, Map map)throws Exception;
 }
