b95d7ce90e6261c977ff14e9e2533eb99b19c3f0
guyabin <guyabin@DESKTOP-BBCVJSH>
2019-04-28 15:46:28 +0800

    1

--- pcitc-base/src/main/java/com/pcitc/base/stp/equipment/FindAppltid.java
+++ pcitc-base/src/main/java/com/pcitc/base/stp/equipment/FindAppltid.java
@@ -4,6 +4,41 @@ public class FindAppltid {
 private String detailid;
 private String equipmentName;
 private String specification;
+private String price;
+private String number;
+private String assetnumber;
+private String placepeople;
+private String sumprice;
+public String getSumprice() {
+	return sumprice;
+}
+public void setSumprice(String sumprice) {
+	this.sumprice = sumprice;
+}
+public String getAssetnumber() {
+	return assetnumber;
+}
+public void setAssetnumber(String assetnumber) {
+	this.assetnumber = assetnumber;
+}
+public String getPlacepeople() {
+	return placepeople;
+}
+public void setPlacepeople(String placepeople) {
+	this.placepeople = placepeople;
+}
+public String getPrice() {
+	return price;
+}
+public void setPrice(String price) {
+	this.price = price;
+}
+public String getNumber() {
+	return number;
+}
+public void setNumber(String number) {
+	this.number = number;
+}
 public String getDetailid() {
 	return detailid;
 }
