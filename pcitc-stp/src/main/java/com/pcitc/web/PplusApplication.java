1a00fc4d64b7b2994d5042615722adc2560ea4f6
t-haifeng.zhang <t-haifeng.zhang@pcitc.com>
2019-05-15 16:52:27 +0800

    oss存储接口调整

--- pcitc-stp/src/main/java/com/pcitc/web/PplusApplication.java
+++ pcitc-stp/src/main/java/com/pcitc/web/PplusApplication.java
@@ -54,7 +54,7 @@ public class PplusApplication extends SpringBootServletInitializer {
 		SpringApplication.run(PplusApplication.class, args);
 	}
 
-	/*@Override
+	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		System.out.println("tomcat外置方式---------------启动");
 		return application.sources(PplusApplication.class);
@@ -71,7 +71,7 @@ public class PplusApplication extends SpringBootServletInitializer {
 			}
 		};
 	}
-	
+	/*
 	@Bean
 	public ServletContextInitializer contextInitializer() {
 	    return new ServletContextInitializer() {
