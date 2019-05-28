bbd7c51f37a264bfbb82a1fcb0d0cbe5f233eece
t-haifeng.zhang <t-haifeng.zhang@pcitc.com>
2019-05-28 09:52:22 +0800

    移动测试

--- pcitc-stp/src/main/java/com/pcitc/web/controller/AdminMobileController.java
+++ pcitc-stp/src/main/java/com/pcitc/web/controller/AdminMobileController.java
@@ -7,10 +7,17 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.alibaba.fastjson.JSONObject;
 import com.pcitc.base.system.SysUser;
 import com.pcitc.web.common.BaseController;
 import com.pcitc.web.utils.DES3Utils;
@@ -24,10 +31,11 @@ import com.pcitc.web.utils.HanaUtil;
 public class AdminMobileController extends BaseController {
 
 	// 访问zuul中的登录方法
-	private static final String LOGIN_URL = "http://pcitc-zuul/auth/login";
-	private static final String GET_USER_INFO_IP = "http://pcitc-zuul/system-proxy/user-provider/user/get-user-byname/";
+	private static final String	LOGIN_URL			= "http://pcitc-zuul/auth/login";
+	private static final String	GET_USER_INFO_IP	= "http://pcitc-zuul/system-proxy/user-provider/user/get-user-byname/";
 
 	/**
+	 * 移动本地测试方法
 	 * @param request
 	 * @param response
 	 * @return
@@ -35,20 +43,16 @@ public class AdminMobileController extends BaseController {
 	 */
 	@RequestMapping(value = "/mobile/indexTest")
 	public String indexMobile(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		String year= HanaUtil.getCurrrentYear();
-	    request.setAttribute("year", year);
-	    
-	    
-		String unitPathId=sysUserInfo.getUnitPath();
-		boolean isKJBPerson=EquipmentUtils.isKJBPerson(unitPathId);
+		String year = HanaUtil.getCurrrentYear();
+		request.setAttribute("year", year);
+
+		String unitPathId = sysUserInfo.getUnitPath();
+		boolean isKJBPerson = EquipmentUtils.isKJBPerson(unitPathId);
 		request.setAttribute("isKJBPerson", isKJBPerson);
-		
-		
-		
 
 		return "/mobile/index";
 	}
-	
+
 	/**
 	 * 科技平台统一身份认证移动首页
 	 * 
@@ -57,27 +61,31 @@ public class AdminMobileController extends BaseController {
 	@RequestMapping(value = "/mobile/index")
 	public String indexMobileStp(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		System.out.println("进入indexMobileStp....");
-		System.out.println("1-----indexMobileStp----identity_token------:" + request.getParameter("identity_token"));
-		System.out.println("2-----indexMobileStp-----identity_key-----:" + request.getParameter("identity_key"));
-		System.out.println("3-----indexMobileStp------Identity_token----:" + request.getParameter("Identity_token"));
-		System.out.println("4-----indexMobileStp------Identity_key----:" + request.getParameter("Identity_key"));
+
+		System.out.println("6都小写-----indexMobileStp-identity_token---------:"+request.getParameter("identity_token"));
+		System.out.println("7都小写-----indexMobileStp-identity_key---------:"+request.getParameter("identity_key"));
+		System.out.println("8都大写-----indexMobileStp-Identity_Key---------:"+request.getParameter("Identity_Key"));
+		System.out.println("9都大写-----indexMobileStp-Identity_Token---------:"+request.getParameter("Identity_Token"));
+		System.out.println("10开头大写-----indexMobileStp-Identity_key---------:"+request.getParameter("Identity_key"));
+		System.out.println("11开头大写-----indexMobileStp-Identity_token---------:"+request.getParameter("Identity_token"));
+		
 		SysUser rsUser = new SysUser();
-		String token = request.getParameter("Identity_token");
+		String token = request.getParameter("Identity_Token");
 		DES3Utils desUtils = new DES3Utils("01qaz2wsx3edc4rfv5tgb6yhn");
-		System.out.println("indexMobileStp==========" + token);
+		System.out.println("indexMobileStp====token======"+token);
 		String key1 = desUtils.des3Decode0(token);
 		Map keymap = desUtils.getAcountByToken0(key1);
 		String username = keymap.get("username").toString();
-		
-		System.out.println("username ==========" + username);
-		/*ResponseEntity<SysUser> rsEntity = this.restTemplate.exchange(GET_USER_INFO_IP + username, HttpMethod.GET, new HttpEntity<Object>(httpHeaders), SysUser.class);
+
+		System.out.println("username =========="+username);
+		ResponseEntity<SysUser> rsEntity = this.restTemplate.exchange(GET_USER_INFO_IP+username, HttpMethod.GET, new HttpEntity<Object>(httpHeaders), SysUser.class);
 		rsUser = rsEntity.getBody();
-		if (rsUser != null) {
+		if (rsUser!=null) {
 			// 用户有哪些菜单权限
 			request.setAttribute("userInfo", rsUser);
 		} else {
 			// 返回权限不足页面
-			System.out.println("LogonController----缺少权限");
+			System.out.println("indexMobileStp----缺少权限");
 			return "no_access";
 		}
 		// 重新登录，覆盖原cookies。cookies中信息都是后续要用的
@@ -90,54 +98,58 @@ public class AdminMobileController extends BaseController {
 		ResponseEntity<JSONObject> responseEntity = this.restTemplate.exchange(LOGIN_URL, HttpMethod.POST, entity, JSONObject.class);
 		JSONObject retJson = responseEntity.getBody();
 
-		System.out.println("-----indexStp----------login token:" + retJson.get("token"));
+		System.out.println("-----indexStp----------login token:"+retJson.get("token"));
 
 		Cookie cookie = new Cookie("token", retJson.getString("token"));
-		cookie.setMaxAge(24 * 60 * 60);// 设置有效期为1天
+		cookie.setMaxAge(1*60*60);// 设置有效期为1天
 		cookie.setPath("/");
-		response.addCookie(cookie);*/
-		
-		
-		String unitPathId=sysUserInfo.getUnitPath();
-		boolean isKJBPerson=EquipmentUtils.isKJBPerson(unitPathId);
+		response.addCookie(cookie);
+
+		String unitPathId = sysUserInfo.getUnitPath();
+		boolean isKJBPerson = EquipmentUtils.isKJBPerson(unitPathId);
 		request.setAttribute("isKJBPerson", isKJBPerson);
-		
-		
 
 		return "/mobile/index";
 	}
 
+	/**
+	 * 登录认证的接口地址
+	 * @param request
+	 * @param response
+	 * @return
+	 */
 	@RequestMapping(value = "/mobile/adToken", method = RequestMethod.POST)
 	public String adToken(HttpServletRequest request, HttpServletResponse response) {
 		String name = "";
 		String pwd = "";
-		System.out.println("1-----adToken----------:" + request + "====" + response);
 		Map param = request.getParameterMap();
-		System.out.println("2-----adToken----------:" + param.isEmpty());
-		System.out.println("3-----adToken----------:" + request.getParameterNames());
-		System.out.println("4-----adToken----identity_token------:" + request.getParameter("identity_token"));
-		System.out.println("5-----adToken----identity_key------:" + request.getParameter("identity_key"));
+		System.out.println("1-----adToken----------:"+param.isEmpty());
+		System.out.println("2-----adToken----------:"+request.getParameterNames());
 		Enumeration paramNames = request.getParameterNames();
 		while (paramNames.hasMoreElements()) {
 			String paramName = (String) paramNames.nextElement();
 			String[] paramValues = request.getParameterValues(paramName);
-			System.out.println("6-----adToken----paramName------:" + paramName);
-			if (paramValues != null) {
-				System.out.println("7-----adToken----------:" + paramValues.length);
-				for (int i = 0; i < paramValues.length; i++) {
-					System.out.println("8-----adToken----------:" + i + "==============" + paramValues[i]);
+			System.out.println("3-----adToken----------:"+paramName);
+			if (paramValues!=null) {
+				System.out.println("4-----adToken----------:"+paramValues.length);
+				for (int i = 0; i<paramValues.length; i++) {
+					System.out.println("5-----adToken----------:"+i+"=============="+paramValues[i]);
 				}
 			}
 		}
-		System.out.println("9-----adToken----Identity_Key------:" + request.getParameter("Identity_Key"));
-		System.out.println("10-----adToken----Identity_Token------:" + request.getParameter("Identity_Token"));
+		System.out.println("6都小写-----adToken-identity_token---------:"+request.getParameter("identity_token"));
+		System.out.println("7都小写-----adToken-identity_key---------:"+request.getParameter("identity_key"));
+		System.out.println("8都大写-----adToken-Identity_Key---------:"+request.getParameter("Identity_Key"));
+		System.out.println("9都大写-----adToken-Identity_Token---------:"+request.getParameter("Identity_Token"));
+		System.out.println("10开头大写-----adToken-Identity_key---------:"+request.getParameter("Identity_key"));
+		System.out.println("11开头大写-----adToken-Identity_token---------:"+request.getParameter("Identity_token"));
 		DES3Utils desUtils = new DES3Utils(request.getParameter("Identity_Key"));
 		String pKey = desUtils.des3Decode(request.getParameter("Identity_Token"));
 		Map<String, String> keymap = desUtils.getAcountByToken(pKey);
 		name = keymap.get("username");
 		pwd = keymap.get("password");
-		
-		System.out.println("11-----adToken----------:" + name + "====" + pwd);
+
+		System.out.println("12-----adToken----------:"+name+"===="+pwd);
 		Cookie c = new Cookie("userInfo", name);
 		c.setPath("/");
 		response.addCookie(c);
