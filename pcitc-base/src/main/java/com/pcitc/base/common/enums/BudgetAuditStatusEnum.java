2d961e1fa4bb94a3f1bf9fe5a1c6d4eae8946d9e
uuy <uuy@DESKTOP-FLDRE7B>
2019-06-03 09:12:15 +0800

    fix bug

--- pcitc-base/src/main/java/com/pcitc/base/common/enums/BudgetAuditStatusEnum.java
+++ pcitc-base/src/main/java/com/pcitc/base/common/enums/BudgetAuditStatusEnum.java
@@ -6,7 +6,7 @@ public enum BudgetAuditStatusEnum {
 	AUDIT_STATUS_START(1,"审批中"),
 	AUDIT_STATUS_REFUSE(-1,"审批驳回"),
 	AUDIT_STATUS_PASS(2,"审批通过"),
-	AUDIT_STATUS_FINAL(3,"最终版本");
+	AUDIT_STATUS_FINAL(3,"最终预算");
 	
 	private BudgetAuditStatusEnum(Integer code, String desc) {
 		this.code = code;
