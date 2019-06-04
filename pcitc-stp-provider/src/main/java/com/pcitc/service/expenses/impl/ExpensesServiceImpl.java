45fbe6f23c3c5df327778c56d7f197578420cf02
t-haifeng.zhang <t-haifeng.zhang@pcitc.com>
2019-06-04 15:29:43 +0800

    获取hana成本数据

--- pcitc-stp-provider/src/main/java/com/pcitc/service/expenses/impl/ExpensesServiceImpl.java
+++ pcitc-stp-provider/src/main/java/com/pcitc/service/expenses/impl/ExpensesServiceImpl.java
@@ -1,5 +1,6 @@
 package com.pcitc.service.expenses.impl;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +12,13 @@ import com.pcitc.base.stp.expenses.ExpensesBills;
 import com.pcitc.base.stp.expenses.ExpensesBillsApproves;
 import com.pcitc.base.stp.expenses.ExpensesBillsItem;
 import com.pcitc.base.stp.expenses.ExpensesBillsPay;
+import com.pcitc.base.stp.expenses.ExpensesPayHana;
+import com.pcitc.base.stp.expenses.ExpensesPayHanaExample;
 import com.pcitc.mapper.expenses.ExpensesBillsApprovesMapper;
 import com.pcitc.mapper.expenses.ExpensesBillsItemMapper;
 import com.pcitc.mapper.expenses.ExpensesBillsMapper;
 import com.pcitc.mapper.expenses.ExpensesBillsPayMapper;
+import com.pcitc.mapper.expenses.ExpensesPayHanaMapper;
 import com.pcitc.service.expenses.ExpensesService;
 
 @Service("expensesService")
@@ -32,6 +36,9 @@ public class ExpensesServiceImpl implements ExpensesService {
 	
 	@Autowired
 	private ExpensesBillsApprovesMapper expensesBillsApprovesMapper;
+	
+	@Autowired
+	private ExpensesPayHanaMapper expensesPayHanaMapper;
 	
 	/**
 	 * @return
@@ -75,4 +82,22 @@ public class ExpensesServiceImpl implements ExpensesService {
 		expensesBillsPayMapper.insertExpensesBillsPayBatch(list);
 	}
 
+	/**
+	 * 批量保存报销对于的erp中的付款明显清单
+	 * @return
+	 */
+	public void insertExpensesPayHanaBatch(List<ExpensesPayHana> list) {
+		expensesPayHanaMapper.insertExpensesPayHanaBatch(list);
+	}
+	
+	/**
+	 * 删除部分数据，重新导入
+	 * @return
+	 */
+	public void deleteExpensesPayHana(HashMap<String, String> hashmap) {
+		ExpensesPayHanaExample ephe = new ExpensesPayHanaExample();
+		String G0GJAHR = hashmap.get("G0GJAHR");
+		ephe.createCriteria().andG0gjahrEqualTo(G0GJAHR);
+		expensesPayHanaMapper.deleteByExample(ephe);
+	}
 }
