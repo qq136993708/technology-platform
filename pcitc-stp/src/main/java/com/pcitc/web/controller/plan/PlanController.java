36af5f30f2e9ad8a458b24424d385768c17ba9ce
t-liaochao.ma <mlc19860417@163.com>
2019-06-26 08:37:03 +0800

    漏洞修复

--- pcitc-stp/src/main/java/com/pcitc/web/controller/plan/PlanController.java
+++ pcitc-stp/src/main/java/com/pcitc/web/controller/plan/PlanController.java
@@ -125,10 +125,31 @@ public class PlanController extends BaseController {
 		request.setAttribute("bak6", sysUserInfo.getUserId());
 		request.setAttribute("bak4", sysUserInfo.getUserDisp());
 		request.setAttribute("closeType", request.getParameter("closeType"));
-
 		return "stp/plan/addPlanPage";
 	}
 
+    /**
+     * 主页任务新增
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/plan/addPlanPageMain")
+    public String pageAddPlanMain(HttpServletRequest request) {
+        Object dataId = request.getParameter("dataId");
+        String flag = "edit";
+        if (dataId == null || "".equals(dataId)) {
+            dataId = UUID.randomUUID().toString().replace("-", "");
+            flag = "add";
+        }
+        request.setAttribute("flag", flag);
+        request.setAttribute("dataId", dataId);
+        request.setAttribute("userName", sysUserInfo.getUserDisp());
+        request.setAttribute("unitName", sysUserInfo.getUnitName());
+        request.setAttribute("bak6", sysUserInfo.getUserId());
+        request.setAttribute("bak4", sysUserInfo.getUserDisp());
+        request.setAttribute("closeType", "1");
+        return "stp/plan/addPlanPage";
+    }
 	/**
 	 * 任务编辑
 	 *
