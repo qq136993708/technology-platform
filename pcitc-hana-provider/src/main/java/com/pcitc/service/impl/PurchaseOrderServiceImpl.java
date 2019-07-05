b995a9b08598b0f2464cc70c219c527b2a72f417
admin <admin@admin-PC>
2019-07-05 12:36:58 +0800

    ok

--- pcitc-hana-provider/src/main/java/com/pcitc/service/impl/PurchaseOrderServiceImpl.java
+++ pcitc-hana-provider/src/main/java/com/pcitc/service/impl/PurchaseOrderServiceImpl.java
@@ -1,15 +1,22 @@
 package com.pcitc.service.impl;
 
-import com.pcitc.mapper.out.PurchaseArrivalMapper;
-import com.pcitc.mapper.out.PurchaseOrderMapper;
-import com.pcitc.service.IPurchaseOrderService;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
-import java.util.List;
+import com.pcitc.base.common.LayuiTableData;
+import com.pcitc.base.common.LayuiTableParam;
+import com.pcitc.base.common.Page;
+import com.pcitc.base.stp.equipment.SrePurchaseOrder;
+import com.pcitc.mapper.equipment.ErpOrderMapper;
+import com.pcitc.mapper.out.PurchaseArrivalMapper;
+import com.pcitc.mapper.out.PurchaseOrderMapper;
+import com.pcitc.service.IPurchaseOrderService;
 
 @Service("purchaseOrderService")
 @Transactional(propagation = Propagation.REQUIRED, readOnly = false, rollbackFor = Exception.class)
@@ -19,6 +26,9 @@ public class PurchaseOrderServiceImpl implements IPurchaseOrderService {
     private PurchaseOrderMapper purchaseOrderMapper;
     @Autowired
     private PurchaseArrivalMapper purchaseArrivalMapper;
+    
+    @Autowired
+    private ErpOrderMapper erpOrderMapper;
     
 
     @Override
@@ -30,4 +40,49 @@ public class PurchaseOrderServiceImpl implements IPurchaseOrderService {
     public List getPurchaseArrivalList(HashMap<String, String> map) {
         return purchaseArrivalMapper.getPurchaseArrivalList(map);
     }
+    
+    
+    
+    
+    /**
+	 * 根据ERP号获取采购订单的数据
+	 */
+    public LayuiTableData getHanaErpOrderPage(LayuiTableParam param)throws Exception
+    {
+  	        //每页显示条数
+	  		int pageSize = param.getLimit();
+	  		int pageNum = param.getPage();
+	  		Page p=new Page(pageNum,pageSize);
+			int start=(pageNum-1)*p.getPageSize();
+	  		String month=(String)param.getParam().get("month");
+	  		String g0PROJCODE=(String)param.getParam().get("code");
+	  		
+	  		
+	  		Map map=new HashMap();
+	  		map.put("start", start);
+	  		map.put("pageSize", pageSize);
+	  		map.put("month", month);
+	  		map.put("g0PROJCODE", g0PROJCODE);
+	  		
+	  		
+	  		
+	  		List<SrePurchaseOrder> list = erpOrderMapper.getErpOrderListByG0PROJCODE(map);
+	  		//Integer totalRecords = erpOrderMapper.getErpOrderCountByG0PROJCODE(map);
+	  		LayuiTableData data = new LayuiTableData();
+	  		data.setData(list);
+	  		data.setCount(1000);
+			
+	  	    return data;
+    }
+    
+    
+    
+    public List<SrePurchaseOrder> getHanaErpOrderList(Map map)throws Exception
+    {
+	  		List<SrePurchaseOrder> list = erpOrderMapper.getErpOrderListByG0PROJCODE(map);
+	  	    return list;
+    }
+    
+    
+    
 }
--- pcitc-stp-provider/src/main/java/com/pcitc/mapper/equipment/SrePurchaseOrderMapper.java
+++ pcitc-stp-provider/src/main/java/com/pcitc/mapper/equipment/SrePurchaseOrderMapper.java
@@ -4,6 +4,7 @@ import com.pcitc.base.common.LayuiTableData;
 import com.pcitc.base.stp.equipment.SrePurchaseOrder;
 
 import java.util.List;
+import java.util.Map;
 
 public interface SrePurchaseOrderMapper {
     int insert(SrePurchaseOrder record);
@@ -15,4 +16,7 @@ public interface SrePurchaseOrderMapper {
     void insertPurchaseOrderList(List<SrePurchaseOrder> srePurchaseOrderList);
 
 	List<SrePurchaseOrder> selectErpnum(String g0projcode);
+	
+	
+	List getErpOrderListByG0PROJCODE(String g0projcode);
 }
