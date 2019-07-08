4cc901533245709834e755582983a5c1f79c237a
uuy <uuy@DESKTOP-FLDRE7B>
2019-07-08 20:39:48 +0800

    fix bug

--- pcitc-stp-provider/src/main/java/com/pcitc/service/laboratory/impl/LaboratoryBasicProjectServiceImpl.java
+++ pcitc-stp-provider/src/main/java/com/pcitc/service/laboratory/impl/LaboratoryBasicProjectServiceImpl.java
@@ -162,6 +162,7 @@ public class LaboratoryBasicProjectServiceImpl implements LaboratoryBasicProject
         {
         	c.andBaseIdEqualTo(param.getParam().get("baseId").toString());
         }
+        System.out.println("------------------------"+param.getParam().get("baseId"));
         example.setOrderByClause("create_date desc");
         return this.findByExample(param, example);
     }
