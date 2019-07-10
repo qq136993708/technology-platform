23f03d2982370a57747018e88cc9150d22e0dad0
uuy <uuy@DESKTOP-FLDRE7B>
2019-07-10 17:15:32 +0800

    base code

--- pcitc-stp/src/main/java/com/pcitc/web/controller/budget/BudgetOrganController.java
+++ pcitc-stp/src/main/java/com/pcitc/web/controller/budget/BudgetOrganController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
 import com.pcitc.base.common.LayuiTableParam;
-import com.pcitc.base.stp.budget.BudgetInfo;
 import com.pcitc.base.stp.budget.BudgetOrgan;
 import com.pcitc.web.common.BaseController;
 
