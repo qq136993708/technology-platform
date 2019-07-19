9b81f35659ecbf8ec401615d025d130a1c996ba2
uuy <uuy@DESKTOP-FLDRE7B>
2019-07-19 11:21:36 +0800

    fix bug

--- pcitc-stp-provider/src/main/java/com/pcitc/service/budget/impl/BudgetOrganItemsServiceImpl.java
+++ pcitc-stp-provider/src/main/java/com/pcitc/service/budget/impl/BudgetOrganItemsServiceImpl.java
@@ -1,17 +1,23 @@
 package com.pcitc.service.budget.impl;
 
-import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.pcitc.base.common.LayuiTableData;
 import com.pcitc.base.common.LayuiTableParam;
 import com.pcitc.base.common.Result;
+import com.pcitc.base.common.enums.DelFlagEnum;
 import com.pcitc.base.stp.budget.BudgetOrganItems;
 import com.pcitc.base.stp.budget.BudgetOrganItemsExample;
+import com.pcitc.base.util.MyBeanUtils;
+import com.pcitc.mapper.budget.BudgetOrganItemsMapper;
 import com.pcitc.service.budget.BudgetOrganItemsService;
 /**
  * 
@@ -23,81 +29,150 @@ import com.pcitc.service.budget.BudgetOrganItemsService;
 public class BudgetOrganItemsServiceImpl  implements BudgetOrganItemsService
 {
 
+	@Autowired
+	public BudgetOrganItemsMapper mapper;
+	
+
 	@Override
-	public BudgetOrganItems selectByPrimaryKey(Serializable id) {
-		// TODO Auto-generated method stub
-		return null;
+	public BudgetOrganItems selectBudgetOrganItems(String dataId) 
+	{
+		return mapper.selectByPrimaryKey(dataId);
 	}
 	@Override
-	public LayuiTableData selectTableData(LayuiTableParam param, BudgetOrganItemsExample e) {
-		// TODO Auto-generated method stub
-		return null;
+	public Result saveBudgetOrganItems(BudgetOrganItems bean) throws Exception 
+	{
+		Boolean status = false;
+		try 
+		{
+			Integer rs = mapper.insert(bean);
+			if(rs > 0) {
+				status = true;
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return new Result(status);
 	}
 	@Override
-	public List<BudgetOrganItems> selectListData(BudgetOrganItemsExample e) {
-		// TODO Auto-generated method stub
-		return null;
+	public Result updateBudgetOrganItems(BudgetOrganItems budgetMoneyTotal) throws Exception 
+	{
+		Boolean status = false;
+		try 
+		{
+			Integer rs = mapper.updateByPrimaryKey(budgetMoneyTotal);
+			if(rs > 0) 
+			{
+				status = true;
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return new Result(status);
 	}
 	@Override
-	public Boolean updateBean(BudgetOrganItems bean) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public Result deleteBudgetOrganItems(String id) throws Exception 
+	{
+		BudgetOrganItems b = mapper.selectByPrimaryKey(id);
+		Boolean status = false;
+		try{
+			if(b != null){
+				Map<String,Object> map = MyBeanUtils.transBean2Map(b);
+				map.put("delFlag", DelFlagEnum.STATUS_DEL.getCode());
+				MyBeanUtils.transMap2Bean(map,b);
+				Integer rs = mapper.updateByPrimaryKey(b);
+				if(rs > 0) {
+					status = true;
+				}
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return new Result(status);
 	}
 	@Override
-	public Boolean deleteBean(Serializable id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public Result deleteBudgetOrganItemsReal(String id) throws Exception 
+	{
+		Boolean status = false;
+		try
+		{
+			Integer rs = mapper.deleteByPrimaryKey(id);
+			if(rs > 0) {
+				status = true;
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return new Result(status);
 	}
 	@Override
-	public Boolean saveBean(BudgetOrganItems bean) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public Result saveOrUpdBudgetOrganItems(BudgetOrganItems bean) throws Exception 
+	{
+		Boolean status =  false;
+		try {
+			BudgetOrganItems old = mapper.selectByPrimaryKey(bean.getDataId());
+			if(old == null) 
+			{
+				return this.saveBudgetOrganItems(bean);
+			}else {
+				MyBeanUtils.copyPropertiesIgnoreNull(bean, old);
+				Integer rs = mapper.updateByPrimaryKey(old);
+				if(rs > 0) {
+					status = true;
+				}
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return new Result(status);
 	}
 	@Override
-	public Boolean saveOrUpdBean(Serializable id, BudgetOrganItems bean) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public List<BudgetOrganItems> selectListBudgetOrganItems() 
+	{
+		BudgetOrganItemsExample example = new BudgetOrganItemsExample();
+		return mapper.selectByExample(example);
 	}
 	@Override
-	public BudgetOrganItems selectBudgetOrganItems(String dataId) {
-		// TODO Auto-generated method stub
-		return null;
+	public LayuiTableData selectTableBudgetOrganItems(LayuiTableParam param) 
+	{
+		BudgetOrganItemsExample example = new BudgetOrganItemsExample();
+		BudgetOrganItemsExample.Criteria c = example.createCriteria();
+		c.andDelFlagEqualTo(DelFlagEnum.STATUS_NORMAL.getCode());
+		
+		example.setOrderByClause("organ_id");
+		return selectTableData(param, example);
 	}
 	@Override
-	public Result saveBudgetOrganItems(BudgetOrganItems bean) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public Result updateBudgetOrganItems(BudgetOrganItems bean) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public Result saveOrUpdBudgetOrganItems(BudgetOrganItems bean) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public Result deleteBudgetOrganItems(String dataId) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public List<BudgetOrganItems> selectListBudgetOrganItems() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public List<BudgetOrganItems> selectListBudgetOrganItemsByBean(BudgetOrganItems bean) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public LayuiTableData selectTableBudgetOrganItems(LayuiTableParam param) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<BudgetOrganItems> selectListBudgetOrganItemsByBean(BudgetOrganItems bean) 
+	{
+		BudgetOrganItemsExample example = new BudgetOrganItemsExample();
+		
+		
+		return mapper.selectByExample(example);
 	}
 	
+	private LayuiTableData selectTableData(LayuiTableParam param,BudgetOrganItemsExample example)
+	{
+		//每页显示条数
+		int pageSize = param.getLimit();
+		//从第多少条开始
+		int pageStart = (param.getPage()-1)*pageSize;
+		//当前是第几页
+		int pageNum = pageStart/pageSize + 1;
+		// 1、设置分页信息，包括当前页数和每页显示的总计数
+		PageHelper.startPage(pageNum, pageSize);
+		
+		List<BudgetOrganItems> list = mapper.selectByExample(example);
+		// 3、获取分页查询后的数据
+		PageInfo<BudgetOrganItems> pageInfo= new PageInfo<BudgetOrganItems>(list);
+		// 3、获取分页查询后的数据
+		LayuiTableData data = new LayuiTableData();
+		data.setData(pageInfo.getList());
+		Long total = pageInfo.getTotal();
+		data.setCount(total.intValue());
+		return data;
+	}
 	
 }
