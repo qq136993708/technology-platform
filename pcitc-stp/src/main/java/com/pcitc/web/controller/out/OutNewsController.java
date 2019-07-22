2220279bab3e9f3162bf66b4fc9ca0ee1e454bce
uuy <uuy@DESKTOP-FLDRE7B>
2019-07-22 09:39:34 +0800

    fix bug

--- pcitc-stp/src/main/java/com/pcitc/web/controller/out/OutNewsController.java
+++ pcitc-stp/src/main/java/com/pcitc/web/controller/out/OutNewsController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.google.gson.JsonObject;
@@ -37,12 +36,9 @@ public class OutNewsController extends BaseController
 	public Object outAppraisalList(@ModelAttribute("param") LayuiTableParam param) {
 		
 		JSONArray jsArr = getDataService(OUT_URL);
+		System.out.println(jsArr.toString());
 		//[{"xxbt":"关于2019年新开科研项目合同（任务书）上报相关事宜的通知","fbsj":"2019-06-03T16:59:22"}]
-		if (jsArr != null) {
-			return jsArr.toString();
-		} else {
-			return null;
-		}
+		return jsArr.toString();
 	}
 	/**
 	 * 获取新闻列表
