4d37f6f9b8ac36f312dafd983624dba41e486322
admin <admin@admin-PC>
2019-07-25 20:48:06 +0800

    ok

--- pcitc-system-provider/src/main/java/com/pcitc/service/job/StpEquipmentLedgerJob.java
+++ pcitc-system-provider/src/main/java/com/pcitc/service/job/StpEquipmentLedgerJob.java
@@ -33,35 +33,25 @@ public class StpEquipmentLedgerJob implements Job, Serializable {
         EquipmentLedgerSysClient equipmentLedgerSysClient = SpringContextUtil.getApplicationContext().getBean(EquipmentLedgerSysClient.class);
         EquipmentLedgerStpClient equipmentLedgerStpClient = SpringContextUtil.getApplicationContext().getBean(EquipmentLedgerStpClient.class);
 
-        String  month="";
         System.out.println("调用feign获取ERP装备台账数据----"+new Date()+"-----------开始"+equipmentLedgerSysClient);
 
         HashMap<String, String> map = new HashMap<String, String>();
 
-        /*SimpleDateFormat format = new SimpleDateFormat();
         Calendar c = Calendar.getInstance();
         c.setTime(new Date());
         c.add(Calendar.MONTH, -1);
-        Date m = c.getTime();
-        String mon = format.format(m);
-        Date date = DateUtil.strToDate(mon, DateUtil.FMT_MM);*/
-        System.out.println("开始_______________________________________________________________");
-        month="201906";
-        System.out.println("结束_______________________________________________________________");
+        Date date = c.getTime();
 
-        map.put("month", month/*DateUtil.dateToStr(date, DateUtil.FMT_MM)*/);
+        map.put("month", DateUtil.dateToStr(date, DateUtil.FMT_MM));
 
         JSONArray jSONArray= equipmentLedgerSysClient.getEquipmentLedgerList(map);
         List<SreEquipmentLedger> resultList = JSONObject.parseArray(jSONArray.toJSONString(), SreEquipmentLedger.class);
 
         if (resultList != null&&resultList.size()!=0)
         {
-            equipmentLedgerStpClient.insertEquipmentLedger(resultList);
+           equipmentLedgerStpClient.insertEquipmentLedger(resultList);
         }
 
         System.out.println("调用feign获取ERP装备台账数据----"+new Date()+"-----------结束"+resultList);
-
-
     }
-
 }
