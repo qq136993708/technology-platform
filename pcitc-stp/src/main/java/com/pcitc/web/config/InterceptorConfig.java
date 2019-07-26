b8fda3616b035be520c108a8e87afebad8abcdad
t-haifeng.zhang <t-haifeng.zhang@pcitc.com>
2019-07-26 14:22:40 +0800

    https升级导致的http

--- pcitc-stp/src/main/java/com/pcitc/web/config/InterceptorConfig.java
+++ pcitc-stp/src/main/java/com/pcitc/web/config/InterceptorConfig.java
@@ -33,7 +33,7 @@ public class InterceptorConfig extends WebMvcConfigurerAdapter {
 		// 添加拦截器(拦截器中只有preHandle返回true时才继续执行下一个拦截器或者controller，否则直接返回)
 		registry.addInterceptor(logInterceptor).addPathPatterns("/**");
 		registry.addInterceptor(tokenInterceptor).addPathPatterns("/**");
-		registry.addInterceptor(csrCheckInterceptor).addPathPatterns("/**");
+		//registry.addInterceptor(csrCheckInterceptor).addPathPatterns("/**");
 		super.addInterceptors(registry);
 	}
 	
