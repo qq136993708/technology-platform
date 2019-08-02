9f4bd34db6f5ffcdcaefd4b2da609c8e742d79a0
YTHT <YTHT@192.168.137.21>
2019-08-02 09:28:15 +0800

    新增信息发布，研究院规划

---
+++ pcitc-stp-provider/src/main/java/com/pcitc/mapper/equipment/SreInformationDeliveryMapper.java
@@ -0,0 +1,24 @@
+package com.pcitc.mapper.equipment;
+
+import java.util.List;
+import java.util.Map;
+
+import com.pcitc.base.stp.equipment.SreInformationDelivery;
+import com.pcitc.base.stp.equipment.SrePlanImplement;
+
+public interface SreInformationDeliveryMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(SreInformationDelivery record);
+
+    int insertSelective(SreInformationDelivery record);
+
+    SreInformationDelivery selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(SreInformationDelivery record);
+
+    int updateByPrimaryKey(SreInformationDelivery record);
+
+	List<SreInformationDelivery> getList(Map map);
+
+}
