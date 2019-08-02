b4ff67f82f5fec8db56effe658a8fda904fea324
uuy <uuy@DESKTOP-FLDRE7B>
2019-08-02 10:43:47 +0800

    fix bug

--- pcitc-stp/src/main/java/com/pcitc/web/utils/OtherUtil.java
+++ pcitc-stp/src/main/java/com/pcitc/web/utils/OtherUtil.java
@@ -16,8 +16,6 @@ import org.springframework.web.client.RestTemplate;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.pcitc.base.hana.report.ScientificInvestment;
-import com.pcitc.base.stp.equipment.SreProjectTask;
 import com.pcitc.base.system.SysNews;
 import com.pcitc.base.system.SysNotice;
 import com.pcitc.base.util.CommonUtil;
@@ -46,7 +44,7 @@ public class OtherUtil {
 		HttpEntity<Map<String, Object>> httpEntity = new HttpEntity<Map<String, Object>>(paramMap,httpHeaders);
 		ResponseEntity<JSONArray> responseEntity = restTemplate.exchange(TOP_NOTICE_LIST, HttpMethod.POST, httpEntity, JSONArray.class);
 		int statusCode = responseEntity.getStatusCodeValue();
-		List<SysNotice> list =new ArrayList();
+		List<SysNotice> list =new ArrayList<SysNotice>();
 		JSONArray jSONArray=null;
 		if (statusCode == 200)
 		{
@@ -64,27 +62,35 @@ public class OtherUtil {
 	
 	public static List<SysNews> getSysNewsPicList(HttpServletRequest request,RestTemplate restTemplate,HttpHeaders httpHeaders)
 	{
-		httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);//设置参数类型和编码
-		Map<String ,Object> paramMap = new HashMap<String ,Object>();
-		paramMap.put("stype", "ROOT_XTGL_XWLX_XWTP");
-		paramMap.put("sysFlag", "ROOT_UNIVERSAL_WEHTHER_YES");
-		System.out.println(">getSysNewsPicList>>>>>>>>>>>>>>>>>>>>参数      stype = "+"ROOT_XTGL_XWLX_XWTP");
-		
-		HttpEntity<Map<String, Object>> httpEntity = new HttpEntity<Map<String, Object>>(paramMap,httpHeaders);
-		ResponseEntity<JSONArray> responseEntity = restTemplate.exchange(TOP_NEW_LIST, HttpMethod.POST, httpEntity, JSONArray.class);
-		int statusCode = responseEntity.getStatusCodeValue();
-		List<SysNews> list =new ArrayList();
-		JSONArray jSONArray=null;
-		if (statusCode == 200)
+		List<SysNews> list =new ArrayList<SysNews>();
+		try 
 		{
-			jSONArray = responseEntity.getBody();
-			list = JSONObject.parseArray(jSONArray.toJSONString(), SysNews.class);
-			for(int i=0;i<list.size();i++)
+			httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);//设置参数类型和编码
+			Map<String ,Object> paramMap = new HashMap<String ,Object>();
+			paramMap.put("stype", "ROOT_XTGL_XWLX_XWTP");
+			paramMap.put("sysFlag", "ROOT_UNIVERSAL_WEHTHER_YES");
+			System.out.println(">getSysNewsPicList>>>>>>>>>>>>>>>>>>>>参数      stype = "+"ROOT_XTGL_XWLX_XWTP");
+			
+			HttpEntity<Map<String, Object>> httpEntity = new HttpEntity<Map<String, Object>>(paramMap,httpHeaders);
+			ResponseEntity<JSONArray> responseEntity = restTemplate.exchange(TOP_NEW_LIST, HttpMethod.POST, httpEntity, JSONArray.class);
+			int statusCode = responseEntity.getStatusCodeValue();
+			
+			JSONArray jSONArray=null;
+			if (statusCode == 200)
 			{
-				SysNews scientificInvestment=list.get(i);
-				System.out.println(">>>>>>>>>>>>>>>>>>>> "+scientificInvestment.getTitle());
+				jSONArray = responseEntity.getBody();
+				list = JSONObject.parseArray(jSONArray.toJSONString(), SysNews.class);
+				for(int i=0;i<list.size();i++)
+				{
+					SysNews scientificInvestment=list.get(i);
+					System.out.println(">>>>>>>>>>>>>>>>>>>> "+scientificInvestment.getTitle());
+				}
 			}
+		}catch(Exception e) 
+		{
+			
 		}
+		
 		return list;
 	}
 	
