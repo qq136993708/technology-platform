c0d3846e9f1e8791c0c56b0ba76ad90273c3aa89
t-liaochao.ma <mlc19860417@163.com>
2019-08-09 15:04:47 +0800

    模板

--- pcitc-stp/src/main/java/com/pcitc/web/controller/system/EmailTemplateController.java
+++ pcitc-stp/src/main/java/com/pcitc/web/controller/system/EmailTemplateController.java
@@ -127,6 +127,7 @@ public class EmailTemplateController extends BaseController {
     @RequestMapping(value = "/emailTemplate/getTableData", method = RequestMethod.POST)
     @ResponseBody
     public Object getTableData(@ModelAttribute("param") LayuiTableParam param) {
+        param.getParam().put("createUserId",sysUserInfo.getUserId());
         HttpEntity<LayuiTableParam> entity = new HttpEntity<LayuiTableParam>(param, this.httpHeaders);
         ResponseEntity<LayuiTableData> responseEntity = this.restTemplate.exchange(LISTPAGE, HttpMethod.POST, entity, LayuiTableData.class);
         LayuiTableData data = responseEntity.getBody();
