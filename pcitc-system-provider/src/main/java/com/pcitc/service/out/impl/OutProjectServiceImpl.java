9bfcc016dc9d7f3d672732014cfab5033e1df7a0
t-haifeng.zhang <t-haifeng.zhang@pcitc.com>
2019-08-12 17:52:39 +0800

    队内排序

--- pcitc-system-provider/src/main/java/com/pcitc/service/out/impl/OutProjectServiceImpl.java
+++ pcitc-system-provider/src/main/java/com/pcitc/service/out/impl/OutProjectServiceImpl.java
@@ -1017,6 +1017,7 @@ public class OutProjectServiceImpl implements OutProjectService {
 		LayuiTableData data = new LayuiTableData();
 
 		List finalList = new ArrayList();
+		List finalListOrder = new ArrayList();
 
 		// 利用合同号查询科研奖励数据，单独查询，联合查询效率太慢
 		StringBuffer sb = new StringBuffer("");
@@ -1093,8 +1094,30 @@ public class OutProjectServiceImpl implements OutProjectService {
 					
 				}
 			}
-
-			// list内部分组排序
+			
+			
+			String xmidFlag = "";
+			List finalListSmall = new ArrayList();
+			// list内部分组排序, 同一个xmid内,承担单位有*在最前面（特殊情况没有*，不考虑排序）
+			for (int i = 0; i < finalList.size(); i++) {
+				HashMap temMap = (HashMap) finalList.get(i);
+				Object temXmid = temMap.get("xmid");
+				if (temXmid != null && !temXmid.toString().equals(xmidFlag)) {
+					// 新的xmid
+					finalListOrder.addAll(finalListSmall);
+					xmidFlag = temXmid.toString();
+					finalListSmall = new ArrayList();
+					finalListSmall.add(temMap);
+				} else {
+					Object define8 = temMap.get("define8");
+					if (define8 != null && define8.toString().contains("*")) {
+						finalListSmall.add(0, temMap);
+					} else {
+						finalListSmall.add(temMap);
+					}
+				}
+			}
+			finalListOrder.addAll(finalListSmall);
 
 		}
 
