87d8686ff5969904938041fc2e118698c940ab3c
uuy <uuy@DESKTOP-FLDRE7B>
2019-09-16 10:33:24 +0800

    add payment

--- pcitc-stp/src/main/java/com/pcitc/web/controller/payment/OutProjectPaymentplanController.java
+++ pcitc-stp/src/main/java/com/pcitc/web/controller/payment/OutProjectPaymentplanController.java
@@ -90,6 +90,53 @@ public class OutProjectPaymentplanController extends BaseController
 		request.setAttribute("dataId", IdUtil.createIdByTime());
 		return "stp/payment/project_paymentplan_edit";
 	}
+	@RequestMapping(method = RequestMethod.GET, value = "/payment/project_payment_main")
+	public Object toPaymentProjectMain(HttpServletRequest request) throws Exception 
+	{
+		Map<String,String> dis = new HashMap<String,String>();
+		dis.put("fylbList", "ROOT_FZJCZX_FYLX");//费用类别
+		dis.put("ktlxList", "ROOT_FZJCZX_KTLX");//课题类型
+		dis.put("jsflList", "ROOT_FZJCZX_JSFL");//技术分类
+		dis.put("jflyList", "ROOT_FZJCZX_GSLXCW");//三级级联：经费来源(公司类型财务)->单位类别->研究院
+		dis.put("zycList", "ROOT_ZGSHJT_ZBJG_KJB");//科技部二级级联： 专业处->专业类别
+		dis.put("fzdwList", "ROOT_FZJCZX_FZDW");//负责单位
+		dis.put("fzlxList", "ROOT_FZJCZX_FZLX");//分组类型
+		dis.put("gsbmbmList", "ROOT_ZGSHJT_ZBJG");//部门
+		
+		Map<String,List<SysDictionary>> map = CommonUtil.getDictionaryByParentCodes(new ArrayList<String>(dis.values()), restTemplate, httpHeaders);
+		for(java.util.Iterator<String> iter = dis.keySet().iterator();iter.hasNext();) 
+		{
+			String key = iter.next();
+			request.setAttribute(key, map.get(dis.get(key)));
+		}
+
+		// 倒推部门-各个处室(汉字)->倒推部门
+		String gsbmbmFlag = CommonUtil.getParameter(request, "gsbmbmFlag", "");// 部门
+		String zycbmFlag = CommonUtil.getParameter(request, "zycbmFlag", "");// 处室
+		String zylbbmFlag = CommonUtil.getParameter(request, "zylbbmFlag", "");// 专业类别
+		
+
+		request.setAttribute("zycbmFlag", zycbmFlag);
+		request.setAttribute("gsbmbmFlag", gsbmbmFlag);
+		request.setAttribute("zylbbmFlag", zylbbmFlag);
+		request.setAttribute("ysnd", DateUtils.dateToStr(new Date(),DateUtils.FMT_YY));
+		// (汉字反查CODE),用于级联: 费用来源define11-单位类别define12-研究院define2
+		
+		return "stp/payment/project_payment_main";
+	}
+	@RequestMapping(method = RequestMethod.GET, value = "/payment/project_payment_edit")
+	public Object toPaymentProjectEdit(HttpServletRequest request) throws Exception 
+	{
+		
+		String nd = request.getParameter("nd");
+		if(nd == null) {
+			nd = DateUtil.format(new Date(), DateUtil.FMT_YYYY);
+		}
+		request.setAttribute("nd", nd);
+		request.setAttribute("projectId", request.getParameter("dataId"));
+		request.setAttribute("dataId", IdUtil.createIdByTime());
+		return "stp/payment/project_payment_edit";
+	}
 	@RequestMapping(method = RequestMethod.GET, value = "/payment/project_paymentnotice_main")
 	public Object toPaymentNoticeProjectMain(HttpServletRequest request) throws Exception 
 	{
@@ -174,9 +221,9 @@ public class OutProjectPaymentplanController extends BaseController
 			}
 			if(obj.getString("define18") == null) 
 			{
-				obj.put("paymentNoticeStatus", "未报销");
+				obj.put("paymentNoticeStatus", "未处理");
 			}else {
-				obj.put("paymentNoticeStatus", "已报销");
+				obj.put("paymentNoticeStatus", "报销中");
 			}
 		}
 		//System.out.println(array.toJSONString());
