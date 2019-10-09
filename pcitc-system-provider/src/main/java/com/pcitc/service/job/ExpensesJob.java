5928736f090990d966b2d4b5512c1059ff19dc27
uuy <uuy@DESKTOP-FLDRE7B>
2019-10-09 15:33:21 +0800

    fix upd

--- pcitc-system-provider/src/main/java/com/pcitc/service/job/ExpensesJob.java
+++ pcitc-system-provider/src/main/java/com/pcitc/service/job/ExpensesJob.java
@@ -1,5 +1,6 @@
 package com.pcitc.service.job;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
@@ -30,74 +31,74 @@ import com.pcitc.utils.RestfulHttpClient;
 public class ExpensesJob implements Job, Serializable {
 
 	private static final long	serialVersionUID	= 1L;
+	
+	
+	private String processGetData(String realStartDate,String realEndDate) throws IOException 
+	{
+		System.out.println(">>>>>>>>>>>>>>获取报销数据开始调用远程接口>>>>>>>>>>>>>");
+		// 调用报销系统数据
+		String url = "http://10.248.36.236/ers/bull/unify/request";
+		// 创建一个请求客户端
+		RestfulHttpClient.HttpClient client = RestfulHttpClient.getClient(url);
+		client.post();
+		
+		Map<String, String> headerMap = new HashMap<String, String>();
+		headerMap.put("Content-Type", "application/json");
+		// 设置全局默认请求头，每次请求都会带上这些请求头
+		RestfulHttpClient.setDefaultHeaders(headerMap);
+		// 添加多个参数请求头
+		client.addHeaders(headerMap);
+		JsonObject jo = new JsonObject();
+		
+		jo.addProperty("executeKey", "sysnPubPaymentBillInfo");
+		jo.addProperty("reqMsg", "{'compCode':'1010','begDate':'"+realStartDate+"','endDate':'"+realEndDate+"'}");
+		Date nowDate = new Date();
+		String reqTime = DateUtil.dateToStr(nowDate, DateUtil.FMT_SS);
+		jo.addProperty("reqTime", reqTime);
+		String signMd5 = MD5Util.MD5Encode("sysnPubPaymentBillInfo"+reqTime+"pubBill");
+		jo.addProperty("sign", signMd5);
+		
+		System.out.println(">>>>请求体参数>>>>>>>>"+jo.toString());
+		// 添加请求体
+		client.body(jo.toString());
+		RestfulHttpClient.HttpResponse response = client.request();
+		
+		// 根据状态码判断请求是否成功
+		if (response.getCode()==200) {
+			// 获取响应内容
+			return response.getContent();
+		}
+		return null;
+	}
+	
+	
 	public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
 		
 		SysJobService sysJobService = SpringContextUtil.getApplicationContext().getBean(SysJobService.class);
 		Date jobBiginTime=new Date();
 		try {
-			
-			System.out.println(">>>>>>>>>>>>>>获取报销数据开始调用远程接口>>>>>>>>>>>>>");
-			// 调用报销系统数据
-			String url = "http://10.248.36.236/ers/bull/unify/request";
-			// 创建一个请求客户端
-			RestfulHttpClient.HttpClient client = RestfulHttpClient.getClient(url);
-			client.post();
-			
-			Map<String, String> headerMap = new HashMap<String, String>();
-			headerMap.put("Content-Type", "application/json");
-			// 设置全局默认请求头，每次请求都会带上这些请求头
-			RestfulHttpClient.setDefaultHeaders(headerMap);
-			// 添加多个参数请求头
-			client.addHeaders(headerMap);
-			JsonObject jo = new JsonObject();
-			
 			ExpenseJobClient expenseJobClient = SpringContextUtil.getApplicationContext().getBean(ExpenseJobClient.class);
 			String str=expenseJobClient.selectMaxUpdate();
 			
 			// 查询最大值，+一个月作为本次的查询条件
-			String startDate = (str == null?"20180201":str);
-			Date temStartDate = DateUtil.strToDate(startDate, DateUtil.FMT_YYYY_DD);
+			String startDateStr = (str == null?"20180201":str);
+			//获取最新日期的下一天
+			Date startDate =DateUtil.getTomorrowDay(DateUtil.strToDate(startDateStr, DateUtil.FMT_YYYY_DD));
+			//获取当前日期的前一天
+			Date endDate = DateUtil.getYesterDay(DateUtil.strToDate(DateUtil.format(new Date(), DateUtil.FMT_YYYY_DD), DateUtil.FMT_YYYY_DD));
 			
-			Date monthDay = DateUtil.getNextMonth(temStartDate);
-			
-			List<DateSegment> ds = DateUtil.getMonthList(monthDay, monthDay);
-			String realStartDate = DateUtil.format(ds.get(0).getStartDate(), DateUtil.FMT_YYYY_DD);
-			String realEndDate = DateUtil.format(ds.get(0).getEndDate(), DateUtil.FMT_YYYY_DD);
-			
-			/*Calendar rightNow1 = Calendar.getInstance();
-			rightNow1.setTime(temStartDate);
-			rightNow1.add(Calendar.DAY_OF_YEAR, 1);// 日期加1天
-			Date temDate1 = rightNow1.getTime();
-
-			Calendar rightNow = Calendar.getInstance();
-			rightNow.setTime(temDate1);
-			rightNow.add(Calendar.MONTH, 1);
-			rightNow.add(Calendar.DAY_OF_YEAR, -1);// 日期加1天
-			Date temDate = rightNow.getTime();
-			String realStartDate = DateUtil.dateToStr(temDate1, DateUtil.FMT_YYYY_DD);
-			String realEndDate = DateUtil.dateToStr(temDate, DateUtil.FMT_YYYY_DD);*/
-			
-
-			jo.addProperty("executeKey", "sysnPubPaymentBillInfo");
-			jo.addProperty("reqMsg", "{'compCode':'1010','begDate':'"+realStartDate+"','endDate':'"+realEndDate+"'}");
-			Date nowDate = new Date();
-			String reqTime = DateUtil.dateToStr(nowDate, DateUtil.FMT_SS);
-			jo.addProperty("reqTime", reqTime);
-			String signMd5 = MD5Util.MD5Encode("sysnPubPaymentBillInfo"+reqTime+"pubBill");
-			jo.addProperty("sign", signMd5);
-			
-			System.out.println(">>>>请求体参数>>>>>>>>"+jo.toString());
-			// 添加请求体
-			client.body(jo.toString());
-			RestfulHttpClient.HttpResponse response;
-			response = client.request();
-			// 根据状态码判断请求是否成功
-			if (response.getCode()==200) {
-				// 获取响应内容
-				String result = response.getContent();
+			List<DateSegment> dtlist = DateUtil.getMonthList(startDate, endDate);
+			for(DateSegment s:dtlist) 
+			{
+				Date start = (s.getStartDate().getTime()<=startDate.getTime()?startDate:s.getStartDate());
+				Date end = (s.getEndDate().getTime()>=endDate.getTime()?endDate:s.getEndDate());
+				
+				String realStartDate = DateUtil.format(start, DateUtil.FMT_YYYY_DD);
+				String realEndDate = DateUtil.format(end, DateUtil.FMT_YYYY_DD);
+				
+				String result = processGetData(realStartDate,realEndDate);
 				expenseJobClient.saveExpenses(result);
 			}
-			
 		} catch (Exception e) {
 			System.out.println(">>>>>>>>>>>>>获取报销数据 调用异常>>>>>>>>>>>>>>");
 			JobKey key = jobExecutionContext.getJobDetail().getKey();
