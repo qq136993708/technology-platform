f69be690f5b221d5be11b5e33b36613359d6c68a
admin <admin@DESKTOP-UA02NUA>
2019-11-18 14:02:33 +0800

    ok

--- kjpt-platform/src/main/java/com/pcitc/web/controller/expert/ExpertIndexController.java
+++ kjpt-platform/src/main/java/com/pcitc/web/controller/expert/ExpertIndexController.java
@@ -9,14 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-@Api(value = "Expert-URL",tags = {"专家库-跳转URL"})
 @Controller
 public class ExpertIndexController {
 	
 	
 	    // 专家列表
 		@RequestMapping(value = "/kjpt/expert/expert_list")
-		@ApiOperation(value = "专家列表", notes = "专家列表")
 		public String expert_list(HttpServletRequest request, HttpServletResponse response) 
 		{
 			return "/kjpt/expert/expert_list";
@@ -24,7 +22,6 @@ public class ExpertIndexController {
 		
 		// 专家增加
 		@RequestMapping(value = "/kjpt/expert/expert_add")
-		@ApiOperation(value = "专家新增页", notes = "专家新增页")
 		public String expert_add(HttpServletRequest request, HttpServletResponse response) 
 		{
 			return "/kjpt/expert/expert_add";
@@ -32,7 +29,6 @@ public class ExpertIndexController {
 		
 		// 专家修改
 		@RequestMapping(value = "/kjpt/expert/expert_edit")
-		@ApiOperation(value = "专家新增页", notes = "专家新增页")
 		public String expert_edit(HttpServletRequest request, HttpServletResponse response) 
 		{
 			return "/kjpt/expert/expert_edit";
@@ -40,7 +36,6 @@ public class ExpertIndexController {
 		
 		// 专家详情
 		@RequestMapping(value = "/kjpt/expert/expert_view")
-		@ApiOperation(value = "专家详情页", notes = "专家详情页")
 		public String expert_view(HttpServletRequest request, HttpServletResponse response) 
 		{
 			return "/kjpt/expert/expert_view";
@@ -48,7 +43,6 @@ public class ExpertIndexController {
 		
 		// 专家风采
 		@RequestMapping(value = "/kjpt/expert/expert_graceful")
-		@ApiOperation(value = "专家风采页", notes = "专家风采页")
 		public String expert_graceful(HttpServletRequest request, HttpServletResponse response) 
 		{
 			return "/kjpt/expert/expert_graceful";
