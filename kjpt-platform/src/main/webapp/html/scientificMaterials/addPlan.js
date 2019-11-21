4251317a66c7cd09a3dad5e2468b8d5500fcc9de
麻金书 <t-jinshu.ma@pcitc.com>
2019-11-21 14:44:06 +0800

    添加科研规划页面，修改common.js；添加统一设置表单disabled方法

---
+++ kjpt-platform/src/main/webapp/html/scientificMaterials/addPlan.js
@@ -0,0 +1,15 @@
+layui.use(['element', 'form', 'jquery', 'formSelects'], function(){
+	var element = layui.element;
+	var $ = layui.jquery;
+	var form = layui.form;
+	
+  var variable = getQueryVariable();
+  if (variable.type === 'see') {
+    // 查看-设置表单元素为disabled
+    setFomeDisabled('formAddPlan', '.disabled');
+    $('.disabled-box').remove();
+    layui.form.render('select');
+    layui.formSelects.disabled();
+  }
+
+});
