2d4b10a580c40080a483fecb893537be1499dd1c
admin <admin@DESKTOP-UA02NUA>
2019-11-25 18:05:50 +0800

    三员管理

--- kjpt-platform/src/main/java/com/pcitc/web/interceptor/TokenInterceptor.java
+++ kjpt-platform/src/main/java/com/pcitc/web/interceptor/TokenInterceptor.java
@@ -2,7 +2,10 @@ package com.pcitc.web.interceptor;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
+import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -11,7 +14,9 @@ import javax.xml.ws.RequestWrapper;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +26,10 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.pcitc.base.common.Constant;
 import com.pcitc.base.common.Result;
+import com.pcitc.base.system.SysLog;
 import com.pcitc.base.system.SysUser;
 import com.pcitc.base.util.HostUtil;
 import com.pcitc.web.common.BaseController;
@@ -31,8 +39,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @Component
-public class TokenInterceptor implements HandlerInterceptor {
+public class TokenInterceptor extends BaseController implements HandlerInterceptor {
 
+	
+	private static final String LOG_ADD_URL = "http://kjpt-zuul/system-proxy/log-provider/add";
 	@Autowired
 	private HttpHeaders httpHeaders;
 	
@@ -46,37 +56,27 @@ public class TokenInterceptor implements HandlerInterceptor {
 			
 			    System.out.println(">>>>>>>>用户请求的URL："+request.getRequestURI());
 			    System.out.println(">>>>>>>>用户请求的IP："+request.getRemoteAddr());
+			    
+				
+				
 			    if(handler instanceof HandlerMethod)
 			    {
 			        HandlerMethod method = (HandlerMethod)handler;
-			        //System.out.println("用户想执行的操作是:"+method.getMethodAnnotation(MyOperation.class).value());
-			        
 			        ApiOperation  apiOperation =method.getMethodAnnotation(ApiOperation.class);
 			        if(apiOperation!=null)
 			        {
 			        	  System.out.println("用户想执行的操作是--------------"+apiOperation.value());
 			        }
-			        
-			        RequestMapping annotation = method.getMethodAnnotation(RequestMapping.class);
-	                //获取当前请求接口中的name属性
-	                String name = annotation.name();
-	              
 			        System.out.println(">>>>>>>>用户请求的类："+method.getMethod().getDeclaringClass().getName());
 			        System.out.println(">>>>>>>用户请求的方法："+method.getMethod().getName());
-			        
 			    }
-				
+			   
 			
 			String path = request.getRequestURI();
-			/*
-			 * if(!doLoginInterceptor(path, basePath) ){//是否进行登陆拦截 return true;
-			 * }
-			 */
 			// 手动设置几个常用页面不能直接访问，在InterceptorConfig文件中也可以批量设置
 			if (path != null && (path.indexOf("index.html") > -1 || path.indexOf("login.html") > -1 || path.indexOf("error.html") > -1)) {
 				System.out.println("手动设置几个常用页面不能直接访问");
 				// 统一身份认证时，重定向到/stpHome, 测试环境是/login
-				resultData(request, response);
 				return false;
 			}
 
@@ -87,18 +87,17 @@ public class TokenInterceptor implements HandlerInterceptor {
 			// 安全设置：归档文件下载
 			response.setHeader("Pragma", "no-cache");
 			response.setHeader("Cache-Control", "no-cache");
-
 			// 默认走这个格式，对于form等格式，自己在处理时特殊处理
 			httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
 			String token = null;
 			SysUser sysUser = null;
 			Cookie[] cookies = request.getCookies();
-			if (cookies == null || cookies.length == 0) {
+			if (cookies == null || cookies.length == 0) 
+			{
 				System.out.println("cookies is null ");
 				// login和index为了开发需要，避开统一身份认证
 				if (!request.getRequestURI().contains("/error") && !request.getRequestURI().contains("/kjptmobile/login") && !request.getRequestURI().contains("/kjptmobile/temIndex") && !request.getRequestURI().contains("/kjptmobile/index") && !request.getRequestURI().contains("/login") && !request.getRequestURI().contains("/index") && !request.getRequestURI().contains("/stpHome") && !request.getRequestURI().equals("/")) {
 					// 统一身份认证时，重定向到/stpHome, 测试环境是/login
-					resultData(request, response);
 					return false;
 				}
 				System.out.println("特殊路径--------------"+request.getRequestURI()+"======="+request.getRemoteAddr());
@@ -149,7 +148,6 @@ public class TokenInterceptor implements HandlerInterceptor {
 				// login和index为了开发需要，避开统一身份认证
 				if (!request.getRequestURI().contains("/error") && !request.getRequestURI().contains("/kjptmobile/login") && !request.getRequestURI().contains("/kjptmobile/temIndex") && !request.getRequestURI().contains("/kjptmobile/index") && !request.getRequestURI().contains("/login") && !request.getRequestURI().contains("/index") && !request.getRequestURI().contains("/stpHome") && !request.getRequestURI().equals("/")) {
 					// 统一身份认证时，重定向到/stpHome, 测试环境是/login
-					resultData(request, response);
 					return false;
 				}
 			}
@@ -165,6 +163,67 @@ public class TokenInterceptor implements HandlerInterceptor {
 
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+	
+		System.out.println(">>>>>>dd>>用户请求的URL："+request.getRequestURI());
+	    System.out.println(">>>>>>dd>>用户请求的IP："+request.getRemoteAddr());
+	    
+	    String url=request.getRequestURI();
+		//Authorization头
+		List<String> list = httpHeaders.get("Authorization");
+		if(list!=null)
+		{
+			JSONArray json = JSONArray.parseArray(JSON.toJSONString(list));
+			System.out.println(">>>>>>>>>>>Authorization:"+json.toString());
+			
+		    SysLog sysLog = new SysLog();
+			sysLog.setId(UUID.randomUUID().toString().replaceAll("-", ""));
+			sysLog.setLogIp(getRemoteHost(request));
+			sysLog.setLogTime(new Date());
+			sysLog.setLogUrl(url);
+			if(url.equals("/index"))
+			{
+				sysLog.setLogType(Constant.LOG_TYPE_LOGIN);//日志类型：1登陆日志，2操作日志，3错误日志
+			}else
+			{
+				sysLog.setLogType(Constant.LOG_TYPE_OPT);
+			}
+			SysUser userInfo = JwtTokenUtil.getUserFromTokenByValue(list.get(0).split(" ")[1]);
+			String userName=userInfo.getUserDisp();
+			//用户类型：1普通用户，2系统管理员，3安全员，4审计员
+			if(userName.equals(Constant.LOG_SYSTEMADMIN))
+			{
+				sysLog.setUserType("2");
+			}else if(userName.equals(Constant.LOG_SECURITYADMIN))
+			{
+				sysLog.setUserType("3");
+			}else if(userName.equals(Constant.LOG_AUDITADMIN))
+			{
+				sysLog.setUserType("4");
+			}else
+			{
+				sysLog.setUserType("1");
+			}
+			sysLog.setUserName(userName); 
+			sysLog.setUserId(userInfo.getUserId());
+			sysLog.setRequestType(request.getMethod());
+			httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
+			this.restTemplate.exchange(LOG_ADD_URL, HttpMethod.POST, new HttpEntity<SysLog>(sysLog, this.httpHeaders), Integer.class);
+	
+		}
+		
+	    if(handler instanceof HandlerMethod)
+	    {
+	        HandlerMethod method = (HandlerMethod)handler;
+	        ApiOperation  apiOperation =method.getMethodAnnotation(ApiOperation.class);
+	        if(apiOperation!=null)
+	        {
+	        	  System.out.println("用户想执行的操作是--------------"+apiOperation.value());
+	        }
+	        System.out.println(">>>>>>>>用户请求的类："+method.getMethod().getDeclaringClass().getName());
+	        System.out.println(">>>>>>>用户请求的方法："+method.getMethod().getName());
+	        //sysLog.setRemarks("用户请求的类："+method.getMethod().getDeclaringClass().getName()+"-用户请求的方法："+method.getMethod().getName());
+	     }
+	    
 	}
 
 	public HttpHeaders getHttpHeaders() {
@@ -175,65 +234,36 @@ public class TokenInterceptor implements HandlerInterceptor {
 		this.httpHeaders = httpHeaders;
 	}
 
-	/**
-	 * procss not user result
-	 * 
-	 * @param request
-	 * @param response
-	 * @param httpHeaders
-	 */
-	private void resultData(HttpServletRequest request, HttpServletResponse response) {
-		try {
-			
-			String accept = request.getHeader("accept");// ajax请求头定义返回类型
-			String clientReqType = request.getHeader("client_req_type");// 自定义
-			//当前主机的网络地址
-			Set<String> hostSet = HostUtil.getLocalHostAddressSet();
-			// 遍历ip，如果有服务器ip的跳stpHome，否则跳/login
-	    	boolean reqFlag = false;
-	    	for (String str : hostSet) {  
-	    	      if (str.equals("10.246.94.84") || str.equals("10.246.94.76") || str.equals("172.16.100.127") || str.equals("172.16.100.8")) {
-	    	    	  reqFlag = true;
-	    	      }
-	    	}
-	    	System.out.println("跳转路径--------------"+request.getRequestURI()+"======="+request.getRemoteAddr());
-			String path = request.getRequestURI();
-			if (!StringUtils.isBlank(clientReqType)) {
-				Result rs = new Result(false, reqFlag?"/stpHome":"/login", "登录超时!", "401");
-				PrintWriter out = response.getWriter();
-				out.println(JSON.toJSON(rs));
-				out.close();
-			} else if (!StringUtils.isBlank(accept) && accept.contains("application/json")) {
-				Result rs = new Result(false, reqFlag?"/stpHome":"/login", "登录超时!", "401");
-				PrintWriter out = response.getWriter();
-				out.println(JSON.toJSON(rs));
-				out.close();
-			} else {
-				response.setHeader("Content-Type", "text/html; charset=utf-8");
-				//response.setHeader("X-Content-Type-Options", "nosniff");
-				PrintWriter out = response.getWriter();
-				out.println("<html>");
-				out.println("<script>");
-				if (!path.contains("/kjptmobile/")) {
-			    	if (reqFlag) {
-			    		out.println("window.open ('" + request.getContextPath() + "/SSO/GLO/Redirect','_top')");
-			    	} else {
-			    		out.println("window.open ('" + request.getContextPath() + "/login','_top')");
-			    	}
-				} else {
-					if (reqFlag) {
-			    		out.println("window.open ('" + request.getContextPath() + "/kjptmobile/index','_top')");
-			    	} else {
-			    		out.println("window.open ('" + request.getContextPath() + "/kjptmobile/login','_top')");
-			    	}
-				}
-				
-				out.println("</script>");
-				out.println("</html>");
-				out.close();
-			}
-		} catch (IOException e) {
-			// e.printStackTrace();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public String getRemoteHost(javax.servlet.http.HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
 		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_CLIENT_IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		return ip.equals("0:0:0:0:0:0:0:1") ? "127.0.0.1" : ip;
 	}
+	
 }
--- kjpt-system-provider/src/main/java/com/pcitc/mapper/system/SysLogMapper.java
+++ kjpt-system-provider/src/main/java/com/pcitc/mapper/system/SysLogMapper.java
@@ -1,34 +1,27 @@
 package com.pcitc.mapper.system;
 
-import java.util.List;
-import org.apache.ibatis.annotations.Param;
 import com.pcitc.base.system.SysLog;
-import com.pcitc.base.system.SysLogExample;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface SysLogMapper {
-    long countByExample(SysLogExample example);
-
-    int deleteByExample(SysLogExample example);
-
-    int deleteByPrimaryKey(String logId);
+    int deleteByPrimaryKey(String id);
 
     int insert(SysLog record);
 
     int insertSelective(SysLog record);
 
-    List<SysLog> selectByExample(SysLogExample example);
-
-    SysLog selectByPrimaryKey(String logId);
-
-    int updateByExampleSelective(@Param("record") SysLog record, @Param("example") SysLogExample example);
-
-    int updateByExample(@Param("record") SysLog record, @Param("example") SysLogExample example);
+    SysLog selectByPrimaryKey(String id);
 
     int updateByPrimaryKeySelective(SysLog record);
 
     int updateByPrimaryKey(SysLog record);
     
-    int insertBatch(List<SysLog> list);
-
-	int deleteSysLogById(@Param("ids")List<String> ids);
+    List getList(Map map);
+    
+    int getCount(Map map);
 }
