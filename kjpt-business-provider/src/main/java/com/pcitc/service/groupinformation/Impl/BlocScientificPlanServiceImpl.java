f44e8d7b8d9d835d3a97f7288bd0395f4ed6cbb1
chen <chenbao19951008foxmail.com>
2019-11-30 11:38:50 +0800

    提交代码

--- kjpt-business-provider/src/main/java/com/pcitc/service/groupinformation/Impl/BlocScientificPlanServiceImpl.java
+++ kjpt-business-provider/src/main/java/com/pcitc/service/groupinformation/Impl/BlocScientificPlanServiceImpl.java
@@ -6,6 +6,7 @@ import com.github.pagehelper.PageInfo;
 import com.pcitc.base.groupinformation.BlocScientificPlan;
 import com.pcitc.base.util.IsEmptyUtil;
 import com.pcitc.mapper.groupinformation.BlocScientificPlanMapper;
+import com.pcitc.service.file.FileCommonService;
 import com.pcitc.service.groupinformation.BlocScientificPlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,9 @@ public class BlocScientificPlanServiceImpl implements BlocScientificPlanService 
     @Autowired
     private BlocScientificPlanMapper blocScientificPlanMapper;
 
+    @Autowired
+    private FileCommonService fileCommonService;
+
 
     @Override
     public BlocScientificPlan load(String id) {
@@ -29,6 +33,10 @@ public class BlocScientificPlanServiceImpl implements BlocScientificPlanService 
     public BlocScientificPlan save(BlocScientificPlan blocScientificPlan) {
 
         IsEmptyUtil.isEmpty(blocScientificPlan.getId());
+
+
+        fileCommonService.updateFileData(blocScientificPlan.getAccessory(), blocScientificPlan.getId());
+
 
         if (load(blocScientificPlan.getId()) == null) {
             blocScientificPlan.setUpdateDate(blocScientificPlan.getUpdateDate());
