9f7439da0490ee249bbecbc3c50397658a44764b
t-hongrui.an <ANHRwsx0715>
2019-12-03 17:21:39 +0800

    成果

--- kjpt-platform/src/main/webapp/kjpt/achieve/applyJS.js
+++ kjpt-platform/src/main/webapp/kjpt/achieve/applyJS.js
@@ -18,6 +18,27 @@ layui.use(['jquery','table', 'form'], function() {
             console.log(tableData, type);
         }
     });
+    /*动态生成元素*/
+    function createElement(param) {
+        httpModule({
+            url: "/sysDictionary-api/getChildsListByCode/"+param.code,
+            type: 'GET',
+            success: function(relData) {
+                if (relData.success === true) {
+                    relData.data.map(function(item){
+                        if(param.element=="option"){
+                            if(param.value==item.numValue){
+                                $("#"+param.id+" tbody tr:eq("+param.index+")").find("."+param.className).append("<option value='"+item.numValue+"' selected='selected'>"+item.name+"</option>")
+                            }else {
+                                $("#"+param.id+" tbody tr:eq("+param.index+")").find("."+param.className).append("<option value='"+item.numValue+"'>"+item.name+"</option>")
+                            }
+                        }
+                    });
+                    form.render()
+                }
+            }
+        });
+    }
     var noTr='<tr class="layui-none">' +
         '                  <td colspan="6"><div style="text-align: center">暂无数据,请添加！</div></td>' +
         '                </tr>';
@@ -28,22 +49,59 @@ layui.use(['jquery','table', 'form'], function() {
         var trHtml='<tr>' +
                 '<td>'+index+'</td>' +
                 '<td><input type="text"  placeholder="请填写姓名" autocomplete="off" class="layui-input"></td>' +
-                '<td>3</td>' +
+                '<td><select name="sex" class="sex">' +
+                    '<option value=""></option>' +
+                    '</select></td>' +
                 '<td><input type="text"  placeholder="请填写..." autocomplete="off" class="layui-input"></td>' +
                 '<td><input type="text"  placeholder="请填写..." autocomplete="off" class="layui-input"></td>' +
                 '<td><a style="color: #F44C4C;cursor: pointer;" class="deleTr">删除</a></td>' +
             '</tr>';
+        createElement({code:'ROOT_KJPT_XB',id:'achieveTable',className:'sex',element:'option',index:index})
         $("#achieveTable tbody").append(trHtml)
-        addTr()
+        deleTr()
     })
-    function addTr() {
-
+    function backfill() {
+        var obj=[{name:'1',sex:"1"},{name:'2',sex:"1"}]
+        if(obj.length>0){
+            var off=$("#achieveTable").find(".layui-none");
+            $(off).hide();
+            obj.map(function (item, index) {
+                var trHtml='<tr>' +
+                    '<td>'+(index+1)+'</td>' +
+                    '<td><input type="text" value="'+item.name+'" placeholder="请填写姓名" autocomplete="off" class="layui-input"></td>' +
+                    '<td><select name="sex" class="sex">' +
+                    '<option value=""></option>' +
+                    '</select></td>' +
+                    '<td><input type="text"  placeholder="请填写..." autocomplete="off" class="layui-input"></td>' +
+                    '<td><input type="text"  placeholder="请填写..." autocomplete="off" class="layui-input"></td>' +
+                    '<td><a style="color: #F44C4C;cursor: pointer;" class="deleTr">删除</a></td>' +
+                    '</tr>';
+                createElement({code:'ROOT_KJPT_XB',id:'achieveTable',className:'sex',element:'option',index:index+1,value:item.sex})
+                $("#achieveTable tbody").append(trHtml)
+            })
+        }
+        deleTr()
     }
-    function deleTr() {
+    backfill()
+    function deleTr(){
+        $(".deleTr").click(function () {
+            $(this).parents("tr").remove()
+            var tr=$("#achieveTable tbody tr")
+            $(tr).map(function (index, item) {
+                if($(this).attr("class")!="layui-none"){
+                    $(this).find("td:nth-child(1)").html(index)
+                }
 
+            });
+        })
     }
     function getTableData(){
-
+        var tr=$("#achieveTable tbody tr")
+        $(tr).map(function (index, item) {
+            if(index!==0){
+                /*获取数据*/
+            }
+        })
     }
     form.on('submit(formSave)', function(data) {
         getTableData()
