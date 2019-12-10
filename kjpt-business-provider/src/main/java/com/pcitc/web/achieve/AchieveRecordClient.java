e7b42b0899904dc80c851f7572858673cc934063
admin <admin@DESKTOP-UA02NUA>
2019-12-10 16:53:28 +0800

    流程

--- kjpt-business-provider/src/main/java/com/pcitc/web/achieve/AchieveRecordClient.java
+++ kjpt-business-provider/src/main/java/com/pcitc/web/achieve/AchieveRecordClient.java
@@ -4,6 +4,8 @@ import com.github.pagehelper.PageInfo;
 import com.pcitc.base.achieve.AchieveBase;
 import com.pcitc.base.achieve.AchieveRecord;
 import com.pcitc.base.achieve.AchieveSubmit;
+import com.pcitc.base.common.Result;
+import com.pcitc.base.workflow.Constants;
 import com.pcitc.service.achieve.AchieveBaseService;
 import com.pcitc.service.achieve.AchieveRecordService;
 import io.swagger.annotations.Api;
@@ -14,17 +16,17 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 /**
- * <p>成果转化</p>
+ * <p>成果转换备案</p>
  * @author ty
  */
 
-@Api(value = "achieveRecord-api", description = "成果转化备案接口")
+@Api(value = "achieveRecord-api", description = "成果转换备案备案接口")
 @RestController
 @RequestMapping(value = "/achieveRecord-api")
 public class AchieveRecordClient {
     @Autowired
     private AchieveRecordService ars;
-    @ApiOperation(value = "load成果转化备案接口", notes = "load成果转化备案接口")
+    @ApiOperation(value = "load成果转换备案备案接口", notes = "load成果转换备案备案接口")
     @RequestMapping(value = "/load/{id}", method = RequestMethod.GET)
     public AchieveRecord load(@PathVariable String id){
         return ars.load(id);
@@ -56,5 +58,45 @@ public class AchieveRecordClient {
     public Integer delete(@PathVariable String ids){
          return ars.delete(ids);
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+	
+	@ApiOperation(value="流程处理-成果转换备案上报",notes="流程处理-成果转换备案上报")
+	@RequestMapping(value = "/task/start_activity/{id}", method = RequestMethod.POST)
+	public Result dealWorkFlow(@PathVariable("id") String id,@RequestBody Map map)throws Exception 
+	{
+		return ars.dealWorkFlow(id,map);
+	}
+	
+	
+	@ApiOperation(value="流程处理-驳回",notes="流程处理-驳回")
+	@RequestMapping(value = "/task/reject/{id}", method = RequestMethod.POST)
+	public Integer taskRejectSreProjectTaskInner(@PathVariable(value = "id", required = true) String id)throws Exception {
+		
+		AchieveRecord ar=ars.load(id); 
+		ar.setAuditStatus(String.valueOf(Constants.FLOW_STATE_SAVE)); 
+		int count=ars.saveAchieveRecord(ar);
+		return count;
+	}
+	
+	@ApiOperation(value="流程处理-同意",notes="流程处理-同意")
+	@RequestMapping(value = "/task/agree/{id}", method = RequestMethod.POST)
+	public Integer taskAgreeSreProjectTaskInner(@PathVariable(value = "id", required = true) String id)throws Exception {
+		
+		AchieveRecord ar=ars.load(id);
+		ar.setAuditStatus(String.valueOf(Constants.FLOW_STATE_DONE));
+		int count=ars.saveAchieveRecord(ar) ;
+		return count;
+	}
+	
 
 }
