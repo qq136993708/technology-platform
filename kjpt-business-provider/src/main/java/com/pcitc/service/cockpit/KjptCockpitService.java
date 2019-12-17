83234d331d6dbc98cfd04e5f3140cf893fbcfc9b
t-chengjia.chen <t-chengjia.chen@pcitc.com>
2019-12-17 09:18:38 +0800

    提交驾驶舱接口

---
+++ kjpt-business-provider/src/main/java/com/pcitc/service/cockpit/KjptCockpitService.java
@@ -0,0 +1,35 @@
+package com.pcitc.service.cockpit;
+
+
+import java.util.List;
+import java.util.Map;
+
+
+public interface KjptCockpitService {
+
+    /**
+     * 知识产权-专利数量按专利类型占比分析
+     */
+    List<Map> knowledgeNumOrType(Map<String, Object> param);
+
+    /**
+     * 知识产权-专利数量年度趋势分析
+     */
+    List<Map> knowledgeNumYearTrend(Map<String, Object> param);
+
+    /**
+     * 知识产权-专利数量按单位同比分析
+     */
+    List<Map> knowledgeNumOrUnit(Map<String, Object> param);
+
+    /**
+     * 科研平台-4大类别
+     */
+    List<Map> numScientificType(Map<String, Object> param);
+
+    /**
+     * 科研平台-科研平台数量按技术领域
+     */
+    List<Map> numScientificTechnology(Map<String, Object> param);
+
+}
