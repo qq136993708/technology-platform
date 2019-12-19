c4d851449fa472e2adc2433c66a80afae1afdbec
zhiss <1002759499@qq.com>
2019-12-19 15:33:44 +0800

    fix:修改科技材料字段名称

--- kjpt-platform/src/main/webapp/html/scientificMaterials/js/scientificPlanJS.js
+++ kjpt-platform/src/main/webapp/html/scientificMaterials/js/scientificPlanJS.js
@@ -62,7 +62,7 @@ setSelectInput();
       return '<a href="planDetails.html?id='+d.id+'" class="layui-table-link">'+d.name+'</a>';
     }}, // authenticateUitlText
     {field: 'nameText', title: '申报单位', sort: true },
-    {field: 'technicalFieldName', title: '研究领域'},
+    {field: 'technicalField', title: '研究领域'},
     {field: 'releaseTime', title: '发布时间',templet: function(d){
       var times = new Date(d.releaseTime);
        return times.getFullYear() + '-' + (times.getMonth()+1) + '-' +times.getDate();
