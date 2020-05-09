df68fb9865cb332683993bb62500267274864dec
admin <admin@DESKTOP-UA02NUA>
2020-05-09 10:25:30 +0800

    ok

--- kjpt-system-provider/src/main/java/com/pcitc/service/job/OutDataToBiJob.java
+++ kjpt-system-provider/src/main/java/com/pcitc/service/job/OutDataToBiJob.java
@@ -16,17 +16,17 @@ import com.pcitc.service.system.SysJobService;
 import com.pcitc.utils.QrtzUtils;
 import com.pcitc.utils.RestfulHttpClient;
 
-//质量系统获取数据
+//领导驾驶舱数据同步
 public class OutDataToBiJob implements Job, Serializable {
 
 	private static final long serialVersionUID = 1L;
-	private static final String DATATOBI = "http://localhost:8080/cockpit/bi-api/dataToBi";
+	private static final String DATATOBI = "http://localhost/cockpit/bi-api/dataToBi";
 	public	SysJobService sysJobService = SpringContextUtil.getApplicationContext().getBean(SysJobService.class);
 	
 
 	@Override
 	public void execute(JobExecutionContext context) {
-		System.out.println("====质量系统获取数据======" + DateUtil.dateToStr(new Date(), DateUtil.FMT_SS) + "-----");
+		System.out.println("====领导驾驶舱数据同步======" + DateUtil.dateToStr(new Date(), DateUtil.FMT_SS) + "-----");
 		RestfulHttpClient.HttpResponse response = null;         //发起请求
 		try {
 			response = RestfulHttpClient.getClient(DATATOBI)
--- kjpt-system-provider/src/main/java/com/pcitc/service/job/OutQualityDataJob.java
+++ kjpt-system-provider/src/main/java/com/pcitc/service/job/OutQualityDataJob.java
@@ -2,20 +2,21 @@ package com.pcitc.service.job;
 
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-import java.util.UUID;
 
-import com.pcitc.utils.QrtzUtils;
-import com.pcitc.utils.RestfulHttpClient;
+import javax.servlet.http.HttpServletRequest;
+
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.pcitc.base.util.DateUtil;
 import com.pcitc.config.SpringContextUtil;
 import com.pcitc.service.system.SysJobService;
+import com.pcitc.utils.QrtzUtils;
+import com.pcitc.utils.RestfulHttpClient;
 
 
 
@@ -23,7 +24,6 @@ import com.pcitc.service.system.SysJobService;
 public class OutQualityDataJob implements Job, Serializable {
 
 	private static final long serialVersionUID = 1L;
-	//private static final String Quality = "http://localhost:8080/qims-api/qualityStatistics";
 	private static final String Quality = "http://localhost/qims-api/qualityStatistics";
 	public	SysJobService sysJobService = SpringContextUtil.getApplicationContext().getBean(SysJobService.class);
 	
@@ -31,6 +31,7 @@ public class OutQualityDataJob implements Job, Serializable {
 	public void execute(JobExecutionContext context) throws JobExecutionException {
 		System.out.println("====质量系统获取数据======" + DateUtil.dateToStr(new Date(), DateUtil.FMT_SS) + "-----");
 		RestfulHttpClient.HttpResponse response = null;         //发起请求
+		
 		try {
 			response = RestfulHttpClient.getClient(Quality)
 					.get()              //设置post请求
@@ -45,11 +46,11 @@ public class OutQualityDataJob implements Job, Serializable {
 			
 		}
 
-		System.out.println(">>>>>>>响应状态码: "+response.getCode());     //响应状态码
+		System.out.println(">>>>>>>响应状态码: "+response.getCode());             //响应状态码
 		System.out.println(">>>>>>>>最终发起请求的地址: "+response.getRequestUrl());//最终发起请求的地址
 		if(response.getCode() == 200){
 			//请求成功
-			System.out.println(">>>>>>>>>>响应内容:"+response.getContent());  //响应内容
+			System.out.println(">>>>>>>>>>响应内容:"+response.getContent());     //响应内容
 		}
 		
 	}
