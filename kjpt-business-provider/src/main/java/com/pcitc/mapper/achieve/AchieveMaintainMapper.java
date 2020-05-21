b384b58822c610770aa0a6d684143179d90d57fe
tianye <809526254@qq.com>
2020-05-21 09:59:37 +0800

    成果维护

--- kjpt-business-provider/src/main/java/com/pcitc/mapper/achieve/AchieveMaintainMapper.java
+++ kjpt-business-provider/src/main/java/com/pcitc/mapper/achieve/AchieveMaintainMapper.java
@@ -1,6 +1,8 @@
 package com.pcitc.mapper.achieve;
 
 import com.pcitc.base.achieve.AchieveMaintain;
+import com.pcitc.base.system.SysSerial;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Map;
@@ -13,7 +15,7 @@ public interface AchieveMaintainMapper {
     AchieveMaintain load(String id);
     void add(AchieveMaintain am);
     void update(AchieveMaintain am);
-    Integer delete(String ids);
+    Integer delete(@Param("ids") String ids);
     List query(Map param);
 
 }
