06eb2d0c9a318c0bde08f7c3d6abd802ff1bdd8b
admin <admin@DESKTOP-UA02NUA>
2020-05-26 20:36:27 +0800

    ok

--- kjpt-base/src/main/java/com/pcitc/base/out/OutPersonVo.java
+++ kjpt-base/src/main/java/com/pcitc/base/out/OutPersonVo.java
@@ -18,6 +18,14 @@ public class OutPersonVo implements Serializable{
 	 private String zdwmc;//单位名称
 	 
 	 
+	 private String zzwjbmc;//职务级别名称
+	 private String zzwjbbm;//职务级别编码
+	 private String zjyzx;//最高学历
+	 private String zprzyjszwjbbm;//聘任专业技术职务级别 编码
+	 private String zprzyjszwjbmc;//聘任专业技术职务级别 名称
+	 
+	    
+	    
 	 
 	 
 	 private String zhdzlsj;//获得专利时间
@@ -25,47 +33,17 @@ public class OutPersonVo implements Serializable{
 	 private String zfmzlsj;//专利名称
 	 private String zzlh;//专利号
 	 private String zzlpm;//专利排名
-	 
-	 
-	    
-	 
-	 
-	public String getSeqnr9009() {
-		return seqnr9009;
+	public String getZjyxlmc() {
+		return zjyxlmc;
 	}
-	public void setSeqnr9009(String seqnr9009) {
-		this.seqnr9009 = seqnr9009;
+	public void setZjyxlmc(String zjyxlmc) {
+		this.zjyxlmc = zjyxlmc;
 	}
-	public String getZdwmc() {
-		return zdwmc;
+	public String getZjyxwmc() {
+		return zjyxwmc;
 	}
-	public void setZdwmc(String zdwmc) {
-		this.zdwmc = zdwmc;
-	}
-	public String getZhdzlsj() {
-		return zhdzlsj;
-	}
-	public void setZhdzlsj(String zhdzlsj) {
-		this.zhdzlsj = zhdzlsj;
-	}
-	
-	public String getZfmzlsj() {
-		return zfmzlsj;
-	}
-	public void setZfmzlsj(String zfmzlsj) {
-		this.zfmzlsj = zfmzlsj;
-	}
-	public String getZzlh() {
-		return zzlh;
-	}
-	public void setZzlh(String zzlh) {
-		this.zzlh = zzlh;
-	}
-	public String getZzlpm() {
-		return zzlpm;
-	}
-	public void setZzlpm(String zzlpm) {
-		this.zzlpm = zzlpm;
+	public void setZjyxwmc(String zjyxwmc) {
+		this.zjyxwmc = zjyxwmc;
 	}
 	public String getPernr() {
 		return pernr;
@@ -109,19 +87,78 @@ public class OutPersonVo implements Serializable{
 	public void setIcnum(String icnum) {
 		this.icnum = icnum;
 	}
-	public String getZjyxlmc() {
-		return zjyxlmc;
+	public String getZdwmc() {
+		return zdwmc;
 	}
-	public void setZjyxlmc(String zjyxlmc) {
-		this.zjyxlmc = zjyxlmc;
+	public void setZdwmc(String zdwmc) {
+		this.zdwmc = zdwmc;
 	}
-	public String getZjyxwmc() {
-		return zjyxwmc;
+	public String getZzwjbmc() {
+		return zzwjbmc;
 	}
-	public void setZjyxwmc(String zjyxwmc) {
-		this.zjyxwmc = zjyxwmc;
+	public void setZzwjbmc(String zzwjbmc) {
+		this.zzwjbmc = zzwjbmc;
+	}
+	public String getZzwjbbm() {
+		return zzwjbbm;
+	}
+	public void setZzwjbbm(String zzwjbbm) {
+		this.zzwjbbm = zzwjbbm;
+	}
+	public String getZjyzx() {
+		return zjyzx;
+	}
+	public void setZjyzx(String zjyzx) {
+		this.zjyzx = zjyzx;
+	}
+	public String getZprzyjszwjbbm() {
+		return zprzyjszwjbbm;
+	}
+	public void setZprzyjszwjbbm(String zprzyjszwjbbm) {
+		this.zprzyjszwjbbm = zprzyjszwjbbm;
+	}
+	public String getZprzyjszwjbmc() {
+		return zprzyjszwjbmc;
+	}
+	public void setZprzyjszwjbmc(String zprzyjszwjbmc) {
+		this.zprzyjszwjbmc = zprzyjszwjbmc;
+	}
+	public String getZhdzlsj() {
+		return zhdzlsj;
+	}
+	public void setZhdzlsj(String zhdzlsj) {
+		this.zhdzlsj = zhdzlsj;
+	}
+	public String getSeqnr9009() {
+		return seqnr9009;
+	}
+	public void setSeqnr9009(String seqnr9009) {
+		this.seqnr9009 = seqnr9009;
+	}
+	public String getZfmzlsj() {
+		return zfmzlsj;
+	}
+	public void setZfmzlsj(String zfmzlsj) {
+		this.zfmzlsj = zfmzlsj;
+	}
+	public String getZzlh() {
+		return zzlh;
+	}
+	public void setZzlh(String zzlh) {
+		this.zzlh = zzlh;
+	}
+	public String getZzlpm() {
+		return zzlpm;
+	}
+	public void setZzlpm(String zzlpm) {
+		this.zzlpm = zzlpm;
 	}
 	 
+	 
+	    
+	 
+	 
+	
 	 
 
 }
