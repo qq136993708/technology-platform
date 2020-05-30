71c8bf0dd060d03032dfa848485d69bf03498e25
xiaoh <1234567@126.com>
2020-05-30 15:39:00 +0800

    bug#7030

--- kjpt-platform/src/main/java/com/pcitc/web/controller/expert/ExpertController.java
+++ kjpt-platform/src/main/java/com/pcitc/web/controller/expert/ExpertController.java
@@ -582,8 +582,8 @@ public class ExpertController extends BaseController {
   	   			}
   	   		}
   	   		
-  	   		    String[] headers = { "专家姓名",  "身份证号",    "性别"  , "出生年份"  ,  "职称"  ,  "职务",  "联系方式" };
-  	   		    String[] cols =    {"name",    "idCardNo","sex",  "age",      "titleStr",   "post","contactWay"};
+  	   		    String[] headers = { "专家姓名",  "身份证号",    "性别"  , "出生年份"  ,  "职称"  ,  "职务", "所在单位", "联系方式" };
+  	   		    String[] cols =    {"name",    "idCardNo","sexStr",  "age",      "titleStr",   "post", "belongUnitStr" ,"contactWay"};
   	   		   
   	   	        // 文件名默认设置为当前时间：年月日时分秒
   	   	        String fileName = "专家表__"+DateFormatUtils.format(new Date(), "ddhhmmss");
